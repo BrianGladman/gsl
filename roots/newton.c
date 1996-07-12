6ba18d0190abb068d6887bb6f61cfe20b68694f1
rosalia <rosalia>
1996-07-12 22:08:17 +0000

    some new stuff

---
+++ roots/newton.c
@@ -0,0 +1,10 @@
+#include <math.h>
+
+double newton_find(double (*fn)(), double (*dfn()), double guess)
+{
+  double x, old_x;
+
+  old_x = x = guess;
+
+  
+}
---
+++ roots/roots.h
@@ -0,0 +1,4 @@
+#ifndef __ROOTS_H__
+#define __ROOTS_H__
+double newton_find(double (*fn)(), double (*dfn()), double guess);
+#endif /* __ROOTS_H__ */
