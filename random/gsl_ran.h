9fc667d7b953cc73e85c49a0c2ee90b46f0fdb61
jt <jt>
1996-10-24 19:28:52 +0000

    removed private gsl_ran_copyState() from this public header file

--- random/gsl_ran.h
+++ random/gsl_ran.h
@@ -14,15 +14,19 @@ void gsl_ran_setRandomState(void *);
 unsigned long gsl_ran_random_wstate(void *);
 double gsl_ran_uniform_wstate(void *);
 void gsl_ran_seed_wstate(void *, int);
-void gsl_ran_copyState(void *, void *);
 
+/* Note: it's kind of ugly to have the gaussian random state defined
+ * so explicitly, if we used void *'s we would lose some typechecking
+ * ability, but it would look better.  maybe if we just called it
+ * gsl_ran_gState, it wouldn't be so bad...  */
 typedef struct {
     int ng;
     double g;
     void *randomState;
 } gsl_ran_gaussianRandomState;
 
-void gsl_ran_copyGaussState(gsl_ran_gaussianRandomState *, gsl_ran_gaussianRandomState *);
+void gsl_ran_copyGaussState(gsl_ran_gaussianRandomState *,
+                            gsl_ran_gaussianRandomState *);
 gsl_ran_gaussianRandomState *gsl_ran_getGaussState(void);
 void gsl_ran_setGaussState(gsl_ran_gaussianRandomState *);
 double gsl_ran_gaussian_wstate(gsl_ran_gaussianRandomState *);
