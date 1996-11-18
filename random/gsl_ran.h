e136f7eb05977ce56e6b1539ec93b2fc0e247bfd
jt <jt>
1996-11-18 19:55:37 +0000

    added shuffle/choose to list of functions

--- random/gsl_ran.h
+++ random/gsl_ran.h
@@ -15,6 +15,8 @@ unsigned long gsl_ran_random_wstate(void *);
 double gsl_ran_uniform_wstate(void *);
 void gsl_ran_seed_wstate(void *, int);
 
+void gsl_ran_printState(void *);
+
 /* Note: it's kind of ugly to have the gaussian random state defined
  * so explicitly, if we used void *'s we would lose some typechecking
  * ability, but it would look better.  maybe if we just called it
@@ -31,5 +33,8 @@ gsl_ran_gaussianRandomState *gsl_ran_getGaussState(void);
 void gsl_ran_setGaussState(gsl_ran_gaussianRandomState *);
 double gsl_ran_gaussian_wstate(gsl_ran_gaussianRandomState *);
 double gsl_ran_gaussian();
+
+int * gsl_ran_shuffle(int N, int *x);
+int * gsl_ran_choose(int K, int N, int *x);
 
 #endif /* _gsl_ran_RANDOM_H_ */
