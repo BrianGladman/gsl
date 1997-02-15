e9be0818b972e232d7a4d7227bffca6e86aa6ed6
jt <jt>
1997-02-15 09:44:02 +0000

    Added Poisson distribution

--- random/gsl_ran.h
+++ random/gsl_ran.h
@@ -37,4 +37,8 @@ double gsl_ran_gaussian();
 int * gsl_ran_shuffle(int N, int *x);
 int * gsl_ran_choose(int K, int N, int *x);
 
+double gsl_ran_exponential(double mu);
+int gsl_ran_poisson(double mu);
+void gsl_ran_poisson_array(double mu, int N, int *p);
+
 #endif /* _gsl_ran_RANDOM_H_ */
