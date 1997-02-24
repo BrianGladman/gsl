37573234ad837193cc72075999f4cbf7add20457
rosalia <rosalia>
1997-02-24 16:09:33 +0000

    new work on siman

--- siman/siman.c
+++ siman/siman.c
@@ -21,13 +21,64 @@
   and so forth.
   */
 
-/* try to do it without unions */
+/* implementation of a basic simulated annealing algorithm */
 void gsl_siman_solve(void *x0_p, gsl_Efunc_t Ef,
 		     gsl_siman_step_t take_step,
 		     gsl_siman_metric_t distance,
 		     gsl_siman_print_t print_position,
 		     size_t element_size,
 		     gsl_siman_params_t params)
+{
+  void *x, *new_x;
+  double E, new_E;
+  int i, done;
+  double T;
+
+  x = (void *) malloc(element_size);
+  new_x = (void *) malloc(element_size);
+
+  gsl_ran_seed(time(0L));
+
+  T = params.t_initial;
+  memcpy(x, x0_p, element_size);
+  done = 0;
+
+  while (!done) {
+    E = Ef(x);
+    for (i = 0; i < params.n_tries-1; ++i) {
+      take_step(new_x, params.step_size);
+      new_E = Ef(new_x);
+      /* now take the crucial step: see if the new point is accepted
+	 or not, as determined by the boltzman probability */
+      if ((new_E < E)
+	  || (exp(-(E - new_E)/(params.k*T)) * gsl_ran_uniform() < 0.5)) {
+	/* yay! take a step */
+	x = new_x;
+      }
+    }
+
+    if (print_position) {	/* see if we need to print stuff as we go */
+      printf("%d %t");
+      print_position(x);
+      print_position(new_x);
+      printf("  %g  %g\n", E, new_E);
+    }
+    if (T < params.t_min) {
+      done = 1;
+    }
+  }
+
+  free(x);
+  free(new_x);
+}
+
+/* implementation of a simulated annealing algorithm with many tries */
+void gsl_siman_solve_many(void *x0_p, gsl_Efunc_t Ef,
+			  gsl_siman_step_t take_step,
+			  gsl_siman_metric_t distance,
+			  gsl_siman_print_t print_position,
+			  size_t element_size,
+			  gsl_siman_params_t params)
 {
   /* the new set of trial points, and their energies and probabilities */
   void *x, *new_x;
