5c2ece25556e66a5685405d8642542f5dc27fb96
jt <jt>
1997-12-12 04:25:48 +0000

    true, these are auto-generated from templates, but i'm checking them in anyway, so a user of 'cvs checkout' can still get them.

---
+++ random/gsl_ran_switch.h
@@ -0,0 +1,9 @@
+### 
+void gsl_ran_use_default(void);
+void gsl_ran_use_taus(void);
+void gsl_ran_use_mrg(void);
+void gsl_ran_use_cmrg(void);
+void gsl_ran_use_uni(void);
+void gsl_ran_use_uni32(void);
+void gsl_ran_use_zuf(void);
+void gsl_ran_use_rand(void);
