d79ce664554338d9f512706e315873fbeef64164
rosalia <rosalia>
1998-02-09 23:11:37 +0000

    small change in roots/test-falsepos

--- siman/siman_test_driver.sh
+++ siman/siman_test_driver.sh
@@ -7,12 +7,12 @@ echo -n "running simulated annealing test #1 (travelling salesman problem)"
 siman_test > siman_test.out1
 SECOND_LAST_ENERGY=`tail -2 siman_test.out1 | head -1 | awk '{print $4}'`
 LAST_ENERGY=`tail -1 siman_test.out1 | awk '{print $4}'`
-#echo $SECOND_LAST_ENERGY $LAST_ENERGY
+# echo " " $SECOND_LAST_ENERGY $LAST_ENERGY
 if [ $SECOND_LAST_ENERGY = $LAST_ENERGY ];
 then
-    echo "... converged"
+    echo "... converged -- PASS"
 else
-    echo "... did NOT converge"
+    echo "... did NOT converge -- FAIL"
     EXIT_STATUS=`expr $EXIT_STATUS + 1`
     echo $EXIT_STATUS
 fi
@@ -21,12 +21,12 @@ echo -n "running simulated annealing test #2 (travelling salesman problem)"
 siman_test > siman_test.out2
 SECOND_LAST_ENERGY=`tail -2 siman_test.out2 | head -1 | awk '{print $4}'`
 LAST_ENERGY=`tail -1 siman_test.out2 | awk '{print $4}'`
-#echo $SECOND_LAST_ENERGY $LAST_ENERGY
-if [ $SECOND_LAST_ENERGY != $LAST_ENERGY ];
+# echo " " $SECOND_LAST_ENERGY $LAST_ENERGY
+if [ $SECOND_LAST_ENERGY = $LAST_ENERGY ];
 then
-    echo "... converged"
+    echo "... converged -- PASS"
 else
-    echo "... did NOT converge"
+    echo "... did NOT converge -- FAIL"
     EXIT_STATUS=`expr $EXIT_STATUS + 1`
     echo $EXIT_STATUS
 fi
