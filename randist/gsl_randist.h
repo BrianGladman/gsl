68c5125570a94e380f00c4c89e0d81e0601e0d79
bjg <bjg>
1998-03-22 00:45:09 +0000

    added gsl_ran_lorentzian and gsl_ran_laplace, changed gsl_ran_gaussian() to gsl_ran_gaussian(void) (ansi C)

--- randist/gsl_randist.h
+++ randist/gsl_randist.h
@@ -5,11 +5,13 @@ int *gsl_ran_shuffle(int N, int *x);
 int *gsl_ran_choose(int K, int N, int *x);
 
 double gsl_ran_exponential(double mu);
+double gsl_ran_lorentzian(double mu);
+double gsl_ran_laplace(double mu);
 int gsl_ran_poisson(double mu);
 
 void gsl_ran_poisson_array(double mu, int N, int *p);
 
-double gsl_ran_gaussian();
+double gsl_ran_gaussian(void);
 
 double gsl_ran_gammaint(int a);
 double gsl_ran_gamma(double a);
