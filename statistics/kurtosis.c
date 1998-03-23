3f8cf2f2a50215f7e80c1144c5277b264f8d212e
bjg <bjg>
1998-03-23 07:04:03 +0000

    added, calculate kurtosis

---
+++ statistics/kurtosis.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <gsl_statistics.h>
+
+double 
+gsl_stats_kurtosis (const double data[], unsigned int n)
+{
+  double mean = gsl_stats_mean(data, n);
+  double est_sd = gsl_stats_est_sd_with_mean(data, n, mean);
+  return gsl_stats_kurtosis_with_mean_and_sd(data, n, mean, est_sd);
+}
+    
+double 
+gsl_stats_kurtosis_with_mean_and_sd (const double data[], unsigned int n,
+				     double mean, double sd)
+{
+  /* takes a dataset and finds the kurtosis */
+
+  double sum = 0, kurtosis;
+  unsigned int i;
+
+  /* find the fourth moment the deviations, normalized by the sd */
+  for (i = 0; i < n; i++)
+    {
+      double x = (data[i] - mean) / sd;
+      sum += x * x * x * x;
+    }
+
+  kurtosis = (sum / n) - 3.0;  /* makes kurtosis zero for a Gaussian */
+
+  return kurtosis;
+}
+
