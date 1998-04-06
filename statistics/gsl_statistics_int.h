6c377a4c6a31df0f16bcd8aca01258b83efa5b97
bjg <bjg>
1998-04-06 01:05:57 +0000

    removed const from pass by value prototypes

--- statistics/gsl_statistics_int.h
+++ statistics/gsl_statistics_int.h
@@ -3,66 +3,44 @@
 
 #include <stddef.h>
 
-double gsl_stats_int_mean (const int data[], const size_t n);
-double gsl_stats_int_variance (const int data[], const size_t n);
-double gsl_stats_int_sd (const int data[], const size_t n);
-double gsl_stats_int_est_variance (const int data[], const size_t n);
-double gsl_stats_int_est_sd (const int data[], const size_t n);
-double gsl_stats_int_absdev (const int data[], const size_t n);
+double gsl_stats_int_mean (const int data[], size_t n);
+double gsl_stats_int_variance (const int data[], size_t n);
+double gsl_stats_int_sd (const int data[], size_t n);
+double gsl_stats_int_est_variance (const int data[], size_t n);
+double gsl_stats_int_est_sd (const int data[], size_t n);
+double gsl_stats_int_absdev (const int data[], size_t n);
 
-double gsl_stats_int_skew (const int data[], const size_t n);
-double gsl_stats_int_kurtosis (const int data[], const size_t n);
+double gsl_stats_int_skew (const int data[], size_t n);
+double gsl_stats_int_kurtosis (const int data[], size_t n);
 
-double 
-gsl_stats_int_variance_with_mean (const int data[], 
-				  const size_t n, 
-				  const double mean);
-double 
-gsl_stats_int_sd_with_mean (const int data[], 
-			    const size_t n, 
-			    const double mean);
-double 
-gsl_stats_int_est_variance_with_mean (const int data[], 
-				      const size_t n, 
-				      const double mean);
-double 
-gsl_stats_int_est_sd_with_mean (const int data[], 
-				const size_t n, 
-				const double mean);
-double 
-gsl_stats_int_absdev_with_mean (const int data[], 
-				const size_t n, 
-				const double mean);
-double 
-gsl_stats_int_skew_with_mean_and_sd (const int data[], 
-				     const size_t n, 
-				     const double mean, 
-				     const double sd);
-double 
-gsl_stats_int_kurtosis_with_mean_and_sd (const int data[],
-					 const size_t n,
-					 const double mean,
-					 const double sd);
+double gsl_stats_int_variance_with_mean (const int data[], size_t n, 
+					 double mean);
+double gsl_stats_int_sd_with_mean (const int data[], size_t n, double mean);
+double gsl_stats_int_est_variance_with_mean (const int data[], size_t n,
+					     double mean);
+double gsl_stats_int_est_sd_with_mean (const int data[], size_t n, double mean);
+double gsl_stats_int_absdev_with_mean (const int data[], size_t n, double mean);
+double gsl_stats_int_skew_with_mean_and_sd (const int data[], size_t n, 
+					    double mean, double sd);
+double gsl_stats_int_kurtosis_with_mean_and_sd (const int data[], size_t n,
+						double mean,  double sd);
 
 double gsl_stats_int_pvariance (const int data1[], const int data2[],
-				const size_t n1, const size_t n2);
+				size_t n1, size_t n2);
 double gsl_stats_int_ttest (const int data1[], const int data2[],
-			    const size_t n1, const size_t n2);
+			    size_t n1, size_t n2);
 
-int /* BASE type */
-gsl_stats_int_max (const int data[], const size_t n);
-int /* BASE type */
-gsl_stats_int_min (const int data[], const size_t n);
+int gsl_stats_int_max (const int data[], size_t n);
+int gsl_stats_int_min (const int data[], size_t n);
 
-size_t gsl_stats_int_max_index (const int data[], const size_t n);
-size_t gsl_stats_int_min_index (const int data[], const size_t n);
+size_t gsl_stats_int_max_index (const int data[], size_t n);
+size_t gsl_stats_int_min_index (const int data[], size_t n);
 
-int gsl_stats_int_sort_data (int data[], const size_t n) ;
+int gsl_stats_int_sort_data (int data[], size_t n) ;
 
 double gsl_stats_int_median_from_sorted_data (const int sorted_data[],
-					      const size_t n) ;
+					      size_t n) ;
 double gsl_stats_int_percentile_from_sorted_data (const int sorted_data[],
-						  const size_t n, 
-						  const double f) ;
+						  size_t n, const double f) ;
 
 #endif /* _GSL_STATISTICS_INT_H */
