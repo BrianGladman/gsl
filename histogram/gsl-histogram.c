ab73bb791b18038cdcb18d19fd7ff788fc716568
bjg <bjg>
1998-04-29 05:53:53 +0000

    added a simple histogram command line program

---
+++ histogram/gsl-histogram.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gsl_histogram.h>
+
+int
+main (int argc, char **argv)
+{
+  double a = 0.0, b = 1.0 ;
+  int n = 10;
+
+  if (argc != 4)
+    {
+      printf ("Usage: gsl-histogram xmin xmax n\n");
+      printf ("Computes a histogram of the data on stdin using n bins from xmin to xmax\n");
+      exit (0);
+    }
+
+  a = atof (argv[1]);
+  b = atof (argv[2]);
+  n = atoi (argv[3]);
+
+  {
+    gsl_histogram * h = gsl_histogram_alloc_uniform (n, a, b) ;
+    int status ;
+    
+    do {
+      double x ;
+      status = fscanf(stdin, "%lg", &x) ;
+      
+      gsl_histogram_increment (h, x) ;
+
+    } while (status == 1) ;
+
+    gsl_histogram_fprintf (stdout, h, "%g") ;
+  }
+  
+  return 0 ;
+}
