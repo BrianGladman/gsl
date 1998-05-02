604382af03dae4fca2ebad82d5ea681c4d7e084e
jungman <jungman>
1998-05-02 04:23:42 +0000

    work in progress

--- specfunc/pow_int.c
+++ specfunc/pow_int.c
@@ -1,10 +1,48 @@
 /* Author:  G. Jungman
  * RCS:     $Id$
  */
+#include <stdlib.h>
 #include <math.h>
 #include <gsl_math.h>
+#include <gsl_errno.h>
 #include "gsl_sf_pow_int.h"
 
+
+int gsl_sf_pow_int_impl(double x, int n, double * result)
+{
+  const int MAXN = 50;
+  const int NGRP = 5;
+
+  if(n < 0) {
+    x = 1./x;
+    n = -n;
+  }
+  
+  if(n == 0) {
+  }
+  else if(n == 1) {
+  }
+  else if(n == 2) {
+  }
+  else if(n == 3) {
+  }
+  else if(n == 4) {
+  }
+  else if(n < NGRP) {
+  
+    double value = 1.;
+    int n_groups;
+    int n_remain;
+    double x5;
+    div_t q = div(n, 5);
+    n_groups = q.quot;
+    n_remain = q.rem;
+  }
+
+  else {
+    *result = pow(x, n);
+  }
+}
 
 double gsl_sf_pow_int(double x, int n)
 {
--- specfunc/test_plotit.c
+++ specfunc/test_plotit.c
@@ -22,6 +22,9 @@ int main(int argc, char * argv[])
   double nu = 100.;
   int n = 2;
 
+printf("%g\n", pow(0., 0.));
+exit(0);
+
 /*
 printf("%g %f %lg\n", (double)M_PI, M_PI, (double)M_PI);
 exit(0);
