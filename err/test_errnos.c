0fa01c8bd8f000cf47dc8475990f5d581307570f
bjg <bjg>
1998-05-10 18:57:33 +0000

    test the gsl_strerror messages to make sure they are unique too

--- err/test_errnos.c
+++ err/test_errnos.c
@@ -38,6 +38,7 @@ main (void)
   CHECK(GSL_EUNDRFLW);
   CHECK(GSL_EOVRFLW);
   CHECK(GSL_ELOSS);
+  CHECK(GSL_EROUND);
 
   for (i = 0 ; i < n ; i++) 
     {
@@ -50,12 +51,32 @@ main (void)
       for (j = 0; j < n; j++)
 	{
 	  if (j != i)
-	    status |= (errors[i].number == errors[j].number);
+	      status |= (errors[i].number == errors[j].number);
 	}
 
       gsl_test (status, "%s is distinct from other error values",
 		errors[i].name);
     }
+
+  for (i = 0; i < n; i++)
+    {
+      int status = 0;
+      int e1 = errors[i].number ;
+      for (j = 0; j < n; j++)
+	{
+	  if (j != i)
+	    {
+	      int e2 = errors[j].number;
+	      status |= (gsl_strerror(e1) == gsl_strerror(e2)) ;
+	    }
+	}
+
+      status |= (gsl_strerror(e1) == gsl_strerror(-1)) ;
+
+      gsl_test (status, "%s has a distinct error message",
+		errors[i].name);
+    }
+
   
   return gsl_test_summary ();
 }
