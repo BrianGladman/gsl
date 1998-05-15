d055c47924a85ce169371e772efa1387b68d3dd7
bjg <bjg>
1998-05-15 03:45:43 +0000

    an inlined max(a,b) function

---
+++ integration/max.h
@@ -0,0 +1,15 @@
+
+inline static double max (double x, double y) ;
+
+inline static double
+max (double x, double y)
+{
+  if (x > y)
+    {
+      return x ;
+    }
+  else
+    {
+      return y ;
+    }
+}
