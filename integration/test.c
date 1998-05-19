35c0cc6c8ceed9f331b5d0c1fc409c330bfe6fb7
bjg <bjg>
1998-05-19 01:55:01 +0000

    work in progress

--- integration/test.c
+++ integration/test.c
@@ -1,29 +1,124 @@
 #include <stdio.h>
 #include <math.h>
 #include <gsl_integration.h>
+#include <gsl_test.h>
 
-double qtest1 (double x) ;
-double g (double x) ;
-double stepfn (double x) ;
+#include "tests.h"
 
 double alpha = 0 ;
 
 int main (void)
 {
   size_t neval ;
-  int status ;
-      
+
   {
     double result = 0, abserr = 0, resabs = 0, resasc = 0 ;
-    quad_rule (f,0.0,1.0,&result,&abserr,&resabs,&resasc) ;
+    double exp_result = 7.716049357767090777E-02;
+    double exp_abserr = 2.990224871000550874E-06;
+    double exp_resabs = 7.716049357767090777E-02;
+    double exp_resasc = 4.434273814139995384E-02;
+
+    alpha = 2.6 ;
+    gsl_integration_qk15 (book1, 0.0, 1.0, 
+			  &result, &abserr, &resabs, &resasc) ;
+    gsl_test_rel(result,exp_result,1e-15,"qk15(book1) matches result") ;
+    gsl_test_rel(abserr,exp_abserr,1e-15,"qk15(book1) matches abserr") ;
+    gsl_test_rel(resabs,exp_resabs,1e-15,"qk15(book1) matches resabs") ;    
+    gsl_test_rel(resasc,exp_resasc,1e-15,"qk15(book1) matches resasc") ;
   }
 
-  gsl_integration_qng(f,0.0,1.0,0.0,1e-5,&result,&abserr,&neval) ;
+
+  {
+    double result = 0, abserr = 0, resabs = 0, resasc = 0 ;
+    double exp_result = 7.716049379303084599E-02;
+    double exp_abserr = 9.424302194248481445E-08;
+    double exp_resabs = 7.716049379303084599E-02;
+    double exp_resasc = 4.434311425038358484E-02;
+
+    alpha = 2.6 ;
+    gsl_integration_qk21 (book1, 0.0, 1.0, 
+			  &result, &abserr, &resabs, &resasc) ;
+    gsl_test_rel(result,exp_result,1e-11,"qk21(book1) matches result") ;
+    gsl_test_rel(abserr,exp_abserr,1e-15,"qk21(book1) matches abserr") ;
+    gsl_test_rel(resabs,exp_resabs,1e-15,"qk21(book1) matches resabs") ;    
+    gsl_test_rel(resasc,exp_resasc,1e-15,"qk21(book1) matches resasc") ;
+  }
+
+
+  {
+    double result = 0, abserr = 0, resabs = 0, resasc = 0 ;
+    double exp_result = 7.716049382494900855E-02;
+    double exp_abserr = 1.713503193600029893E-09;
+    double exp_resabs = 7.716049382494900855E-02;
+    double exp_resasc = 4.427995051868838933E-02;
+
+    alpha = 2.6 ;
+    gsl_integration_qk31 (book1, 0.0, 1.0, 
+			  &result, &abserr, &resabs, &resasc) ;
+    gsl_test_rel(result,exp_result,1e-15,"qk31(book1) matches result") ;
+    gsl_test_rel(abserr,exp_abserr,1e-15,"qk31(book1) matches abserr") ;
+    gsl_test_rel(resabs,exp_resabs,1e-15,"qk31(book1) matches resabs") ;    
+    gsl_test_rel(resasc,exp_resasc,1e-15,"qk31(book1) matches resasc") ;
+  }
+
+
+  {
+    double result = 0, abserr = 0, resabs = 0, resasc = 0 ;
+    double exp_result = 7.716049382681375302E-02;
+    double exp_abserr = 9.576386660975511224E-11;
+    double exp_resabs = 7.716049382681375302E-02;
+    double exp_resasc = 4.421521169637691873E-02;
+    
+    alpha = 2.6 ;
+    gsl_integration_qk41 (book1, 0.0, 1.0, 
+			  &result, &abserr, &resabs, &resasc) ;
+    gsl_test_rel(result,exp_result,1e-15,"qk41(book1) matches result") ;
+    gsl_test_rel(abserr,exp_abserr,1e-15,"qk41(book1) matches abserr") ;
+    gsl_test_rel(resabs,exp_resabs,1e-15,"qk41(book1) matches resabs") ;    
+    gsl_test_rel(resasc,exp_resasc,1e-15,"qk41(book1) matches resasc") ;
+  }
+
+  {
+    double result = 0, abserr = 0, resabs = 0, resasc = 0 ;
+    double exp_result = 7.716049382708510540E-02;
+    double exp_abserr = 1.002079980317363772E-11;
+    double exp_resabs = 7.716049382708510540E-02;
+    double exp_resasc = 4.416474291216854892E-02;
+
+    alpha = 2.6 ;
+    gsl_integration_qk51 (book1, 0.0, 1.0, 
+			  &result, &abserr, &resabs, &resasc) ;
+    gsl_test_rel(result,exp_result,1e-15,"qk51(book1) matches result") ;
+    gsl_test_rel(abserr,exp_abserr,1e-15,"qk51(book1) matches abserr") ;
+    gsl_test_rel(resabs,exp_resabs,1e-15,"qk51(book1) matches resabs") ;    
+    gsl_test_rel(resasc,exp_resasc,1e-15,"qk51(book1) matches resasc") ;
+  }
+
+
+  {
+    double result = 0, abserr = 0, resabs = 0, resasc = 0 ;
+    double exp_result = 7.716049382713800753E-02;
+    double exp_abserr = 1.566060362296155616E-12;
+    double exp_resabs = 7.716049382713800753E-02;
+    double exp_resasc = 4.419287685934316506E-02;
+    
+    alpha = 2.6 ;
+    gsl_integration_qk61 (book1, 0.0, 1.0, 
+			  &result, &abserr, &resabs, &resasc) ;
+    gsl_test_rel(result,exp_result,1e-15,"qk61(book1) matches result") ;
+    gsl_test_rel(abserr,exp_abserr,1e-15,"qk61(book1) matches abserr") ;
+    gsl_test_rel(resabs,exp_resabs,1e-15,"qk61(book1) matches resabs") ;    
+    gsl_test_rel(resasc,exp_resasc,1e-15,"qk61(book1) matches resasc") ;
+  }
+
+
+#ifdef JUNK
+ gsl_integration_qng(f,0.0,1.0,0.0,1e-5,&result,&abserr,&neval) ;
   
   printf("qng: result = %.18g, abserr = %.18g, neval = %d\n",
 	 result, abserr, neval) ;
 
-#ifdef JUNK
+
   {
     double alist[1000], blist[1000], rlist[1000], elist[1000];
     size_t iord[1000] ;
@@ -35,7 +130,6 @@ int main (void)
     printf("qage: result = %.18g, abserr = %.18g, neval = %d\n",
 	   result, abserr, neval) ;
   }
-#endif
 
   {
     double alist[1000], blist[1000], rlist[1000], elist[1000];
@@ -50,7 +144,6 @@ int main (void)
     printf("status=%d\n",status) ;
   }
 
-#ifdef JUNK
   {
     double alist[1000], blist[1000], rlist[1000], elist[1000];
     size_t iord[1000] ;
@@ -79,27 +172,6 @@ int main (void)
   }
 #endif
 
-  return 0 ;
+  return gsl_test_summary() ;
 } 
-
-double qtest1 (double x) {
-  return pow(x,alpha) * log(1/x) ;
-}
-
-double g (double x) {
-  return 1/sqrt(fabs(x*x + 2*x - 2)) ;
-}
-
-
-double stepfn (double x) {
-  if (x < 1.0/3.0)
-    {
-      return 0 ;
-    }
-  
-  return 1 ;
-}
-
-
-
 
