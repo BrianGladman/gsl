4b2eadca794ddcfd3e675631054c599ffb581e80
bjg <bjg>
1998-06-20 22:31:24 +0000

    the old VAX random number generator

---
+++ rng/vax.c
@@ -0,0 +1,67 @@
+#include <config.h>
+#include <stdlib.h>
+#include <gsl_rng.h>
+
+/* This is the old vax generator MTH$RANDOM. The sequence is,
+   
+         x_{n+1} = (a x_n + c) mod m
+
+   with a = 69069, c = 1 and m = 2^32. The seed specifies the initial
+   value, x_1.
+
+   The theoretical value of x_{10001} is 3051034865.
+
+   The period of this generator is 2^32. */
+
+unsigned long int vax_get (void * vstate);
+void vax_set (void * state, unsigned int s);
+void vax_set_with_state (void * vstate, const void * vinit_state,
+			 unsigned int s);
+
+static const int a = 69069 ;
+static const int c = 1 ;
+static const unsigned int m = 4294967295UL ;
+static const int q = 62183 ;
+static const int r = 49669 ;
+
+typedef struct {
+  unsigned int x;
+} vax_state_t ;
+
+unsigned long int vax_get (void *vstate)
+{
+    vax_state_t * state = (vax_state_t *)vstate;
+
+    const unsigned int x = state->x ;
+
+    const int h = x / q ;
+    const int t = a * (x - h * q) - h * r + c;
+
+    if (t < 0) 
+      {
+	state->x = t + m + 1;
+      }
+    else
+      {
+	state->x = t ;
+      }
+    
+    return state->x;
+}
+
+void vax_set(void * vstate, unsigned int s)
+{
+  vax_state_t * state = (vax_state_t *) vstate;
+  
+  state->x = s ;
+
+  return;
+}
+
+static const gsl_rng_type vax_type = { "vax",  /* name */
+				       4294967295UL,  /* RAND_MAX */
+				       sizeof(vax_state_t), 
+				       &vax_set, 
+				       &vax_get } ;
+
+const gsl_rng_type * gsl_rng_vax = &vax_type ;
