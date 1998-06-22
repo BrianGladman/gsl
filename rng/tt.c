bddc16cc36b8be48b52f2df003c7e6f6eb1dbd04
bjg <bjg>
1998-06-22 07:23:18 +0000

    TT800 generator

---
+++ rng/tt.c
@@ -0,0 +1,100 @@
+#include <config.h>
+#include <stdlib.h>
+#include <gsl_rng.h>
+
+/*  */
+
+
+
+unsigned long int tt_get (void * vstate);
+void tt_set (void * state, unsigned long int s);
+
+#define N 25
+#define M 7
+
+typedef struct {
+  int n ;
+  unsigned long int x[N] ;
+} tt_state_t ;
+
+unsigned long int tt_get (void *vstate)
+{
+    tt_state_t * state = (tt_state_t *)vstate;
+
+    /* this is the magic vector, a */
+
+    const unsigned long mag01[2]={ 0x00000000, 0x8ebfd028UL } ; 
+    unsigned long int y ;
+    unsigned long int * const x = state->x ;
+    int n = state->n ;
+
+    if (n >= N)
+      {
+	int i;
+	for (i=0;i<N-M;i++) {
+	  x[i] = x[i+M] ^ (x[i] >> 1) ^ mag01[x[i] % 2];
+	}
+	for (; i<N;i++) {
+	  x[i] = x[i+(M-N)] ^ (x[i] >> 1) ^ mag01[x[i] % 2];
+	} ;
+	n = 0;
+      }
+   
+    y = x[n];
+    y ^= (y <<  7) & 0x2b5b2500UL; /* s and b, magic vectors */
+    y ^= (y << 15) & 0xdb8b0000UL; /* t and c, magic vectors */
+    y &= 0xffffffffUL; /* you may delete this line if word size = 32 */
+
+    /* The following line was added by Makoto Matsumoto in the 1996
+       version to improve lower bit's correlation.  Delete this line
+       to use the code published in 1994.  */
+    
+    y ^= (y >> 16); /* added to the 1994 version */
+    
+    state->n = n + 1;
+
+    return y;
+}
+
+void tt_set(void * vstate, unsigned long int s)
+{
+  tt_state_t * state = (tt_state_t *) vstate;
+
+  const tt_state_t init_state = { 0, 
+				  { 0x95f24dabUL, 0x0b685215UL, 0xe76ccae7UL, 
+				    0xaf3ec239UL, 0x715fad23UL, 0x24a590adUL, 
+				    0x69e4b5efUL, 0xbf456141UL, 0x96bc1b7bUL, 
+				    0xa7bdf825UL, 0xc1de75b7UL, 0x8858a9c9UL,
+				    0x2da87693UL, 0xb657f9ddUL, 0xffdc8a9fUL, 
+				    0x8121da71UL, 0x8b823ecbUL, 0x885d05f5UL, 
+				    0x4e20cd47UL, 0x5a9ad5d9UL, 0x512c0c03UL, 
+				    0xea857ccdUL, 0x4cc1d30fUL, 0x8891a8a1UL, 
+				    0xa6b7aadbUL } };
+
+ 
+  if (s == 0)  /* default seed is given explicitly in the original code */
+    {
+      *state = init_state ;
+    }
+  else
+    {
+      int i ;
+
+      state->n = 0 ;
+
+      state->x[0] = s & 0xffffffffUL;
+
+      for (i = 1; i < N; i++)
+	state->x[i] = (69069 * state->x[i - 1]) & 0xffffffffUL;
+    }
+  
+  return;
+}
+
+static const gsl_rng_type tt_type = { "tt800",  /* name */
+				       4294967295UL,  /* RAND_MAX */
+				       sizeof(tt_state_t), 
+				       &tt_set, 
+				       &tt_get } ;
+
+const gsl_rng_type * gsl_rng_tt800 = &tt_type ;
