2d61022a317a72faa9d3d150a8c30de431a960fa
booth <booth>
1998-07-06 23:18:58 +0000

    cut down on use malloc calls by reusing vectors.

--- monte/miser.c
+++ monte/miser.c
@@ -23,161 +23,152 @@
 #include <gsl_miser.h>
 #include <gsl_monte_plain.h>
 
-#define PFAC 0.1
-#define TINY 1.0e-30
-#define BIG 1.0e30
-
 #define SQR(a) ((a)*(a))
 #define myMAX(a,b) ((a) >= (b) ? (a) : (b))
 #define myMIN(a,b) ((a) <= (b) ? (a) : (b))
 
-#define COPYSIGN(a,b) ((b) >= 0.0 ? fabs(a) : -fabs(a))
-
+/* these should be in the state structure */
 size_t min_calls = 15;
 size_t min_calls_per_bisection = 60;
-  
+double precond_frac =  0.1;
+double ALPHA = 2.0/3.0; 
 double dither;
-
-static long iran;
-
-void ranpt(const gsl_rng * r, gsl_vector* pt, double* xl, double* xh, size_t n);
-
-inline void ranpt(const gsl_rng * r, gsl_vector* pt, double* xl, double* xh, size_t n)
-{
-  size_t j;
-
-  for (j = 0; j < n; j++)
-    gsl_vector_set(pt, j, xl[j] + (xh[j] - xl[j])*gsl_rng_uniform(r));
-}
 
 
 int gsl_monte_miser(const gsl_rng * r, 
-		    double (*func)(double []), double xl[], double xh[], 
+		    double (*func)(double []), double xl[], double xu[], 
 		    size_t num_dim, size_t calls, double *res, double *err)
 {
   int status = 0;
 
-  gsl_vector *xl_temp, *xh_temp;
+  gsl_vector *xl_tmp, *xu_tmp;
 
-  size_t n, npre, calls_l, calls_r;
-  size_t j;
-  int j_bisect;
+  size_t n, pre_calls, calls_l, calls_r;
+  size_t i;
+  int i_bisect;
   double res_l, err_l;
-  double frac_l, fval;
-  double rgn_l, rgn_m, rgn_r, s, sig_l, sig_l_bisect, sig_r, sig_r_bisect;
+  double fraction_l;
+  double f;
+  double xbi_l, xbi_m, xbi_r, s;
+  double sigma_l, sigma_r;
+  double weight_l, weight_r;
   double sum, sum_bisect;
 
   gsl_vector *fmax_l, *fmax_r, *fmin_l, *fmin_r;
-  gsl_vector *pt, *rmid;
+  gsl_vector *x, *x_mid;
 
-  pt = gsl_vector_alloc(num_dim);
+  x = gsl_vector_alloc(num_dim);
 
   if (calls < min_calls_per_bisection) {
-    status = gsl_monte_plain(r, func, xl, xh, num_dim, calls, res, err);
+    status = gsl_monte_plain(r, func, xl, xu, num_dim, calls, res, err);
   }
   else {
-    rmid = gsl_vector_alloc(num_dim);
+    pre_calls = myMAX((size_t)(calls*precond_frac), min_calls);
 
-    npre = myMAX((size_t)(calls*PFAC), min_calls);
-
+    x_mid = gsl_vector_alloc(num_dim);
     fmax_l = gsl_vector_alloc(num_dim);
     fmax_r = gsl_vector_alloc(num_dim);
     fmin_l = gsl_vector_alloc(num_dim);
     fmin_r = gsl_vector_alloc(num_dim);
 
-    /* bisect the integration region, with some fuzz */
-    for (j = 0; j < num_dim; j++) {
+    for (i = 0; i < num_dim; i++) {
 
-      iran = (iran*2661 + 36979) % 175000;
-      s = COPYSIGN(dither, (double)(iran - 87500));
+      /* flip a coin to bisect the integration region with some fuzz */
+      s = (0.5 - gsl_rng_uniform(r) >= 0.0 ? dither : -dither ); 
+      x_mid->data[i] = (0.5 + s)*xl[i] + (0.5 - s)*xu[i];
 
-      gsl_vector_set(rmid, j, (0.5 + s)*xl[j] + (0.5 - s)*xh[j]);
-
-      fmin_l->data[j] = fmin_r->data[j] = BIG;
-      fmax_l->data[j] = fmax_r->data[j] = -BIG;
+      fmin_l->data[i] = fmin_r->data[i] = DBL_MAX;
+      fmax_l->data[i] = fmax_r->data[i] = -DBL_MAX;
     }
     
-    /* Compute some function values to find the half-spaces with the 
-       largest variance.  First we find the largest and smallest value
-       in each half..
+    /* 
+       The idea is to chose the direction to bisect based on which will
+       give the smallest total variance.  We could (and may do so later)
+       use MC to compute these variances.  But the NR guys simply estimate
+       the variances by finding the min and max function values 
+       for each half-region for each bisection.
     */
-    for (n = 1; n <= npre; n++) {
-      ranpt(r, pt, xl, xh, num_dim);
-
-      fval = (*func)(pt->data);
-      for (j = 0; j < num_dim; j++) {
-	if (pt->data[j] <= rmid->data[j]) {
-	  fmin_l->data[j] = myMIN(fmin_l->data[j], fval);
-	  fmax_l->data[j] = myMAX(fmax_l->data[j], fval);
+    for (n = 1; n <= pre_calls; n++) {
+      for (i = 0; i < num_dim; i++)
+	x->data[i] = xl[i] + (xu[i] - xl[i])*gsl_rng_uniform(r);
+      f = (*func)(x->data);
+      for (i = 0; i < num_dim; i++) {
+	if (x->data[i] <= x_mid->data[i]) {
+	  fmin_l->data[i] = myMIN(fmin_l->data[i], f);
+	  fmax_l->data[i] = myMAX(fmax_l->data[i], f);
 	}
 	else {
-	  fmin_r->data[j] = myMIN(fmin_r->data[j], fval);
-	  fmax_r->data[j] = myMAX(fmax_r->data[j], fval);
+	  fmin_r->data[i] = myMIN(fmin_r->data[i], f);
+	  fmax_r->data[i] = myMAX(fmax_r->data[i], f);
 	}
       }
     }
     /* Now find direction with the largest variance */
-    sum_bisect = BIG;
-    j_bisect = -1;
-    sig_l_bisect = sig_r_bisect = 1.0;
-    for (j = 0; j < num_dim;j++) {
-      if (fmax_l->data[j] > fmin_l->data[j] && 
-	  fmax_r->data[j] > fmin_r->data[j]) {
-	sig_l = myMAX(TINY, pow(fmax_l->data[j] - fmin_l->data[j], 2.0/3.0));
-	sig_r = myMAX(TINY, pow(fmax_r->data[j] - fmin_r->data[j], 2.0/3.0));
-	sum = sig_l + sig_r;
+    sum = 0;
+    sum_bisect = DBL_MAX;
+    i_bisect = -1;
+    weight_l = weight_r = 1.0;
+
+    for (i = 0; i < num_dim;i++) {
+      if (fmax_l->data[i] > fmin_l->data[i] && 
+	  fmax_r->data[i] > fmin_r->data[i]) {
+	sigma_l = myMAX(GSL_MACH_EPS, fmax_l->data[i] - fmin_l->data[i]);
+	sigma_r = myMAX(GSL_MACH_EPS, fmax_r->data[i] - fmin_r->data[i]);
+	sum = pow(sigma_l, ALPHA) + pow(sigma_r, ALPHA);
+
 	if (sum <= sum_bisect) {
 	  sum_bisect = sum;
-	  j_bisect = j;
-	  sig_l_bisect = sig_l;
-	  sig_r_bisect = sig_r;
+	  i_bisect = i;
+	  weight_l = pow(sigma_l, ALPHA);
+	  weight_r = pow(sigma_r, ALPHA);
 	}
       }
     }
 
-    gsl_vector_free(fmin_r);
-    gsl_vector_free(fmin_l);
-    gsl_vector_free(fmax_r);
-    gsl_vector_free(fmax_l);
-
-    if (j_bisect < 0) 
-      j_bisect = (num_dim*iran)/175000; /* All were same, so chose
-					   direction at random */
-    rgn_l = xl[j_bisect];
-    rgn_m = rmid->data[j_bisect];
-    rgn_r = xh[j_bisect];
-    
-    frac_l = fabs((rgn_m - rgn_l)/(rgn_r - rgn_l));
-    calls_l = (size_t)
-      (min_calls + (calls - npre - 2*min_calls)*frac_l*sig_l_bisect
-       /(frac_l*sig_l_bisect + (1.0 - frac_l)*sig_r_bisect));
-    calls_r = calls - npre - calls_l;
-
-    xl_temp = gsl_vector_alloc(num_dim);
-    xh_temp = gsl_vector_alloc(num_dim);
-
-    for (j = 0; j < num_dim; j++) {
-      xl_temp->data[j] = xl[j];
-      xh_temp->data[j] = xh[j];
+    if (i_bisect < 0) {
+      /* All were same, so chose direction at random */
+      
+      i_bisect = num_dim*gsl_rng_uniform(r); 
     }
 
-    xh_temp->data[j_bisect] = rmid->data[j_bisect];
-    status = gsl_monte_miser(r, func, xl_temp->data, xh_temp->data, 
+    xbi_l = xl[i_bisect];
+    xbi_m = x_mid->data[i_bisect];
+    xbi_r = xu[i_bisect];
+
+    /* get the actual fractional sizes of the two "halves" */
+    fraction_l = fabs((xbi_m - xbi_l)/(xbi_r - xbi_l));
+    calls_l = min_calls;
+    calls_l += (size_t)
+      (calls - pre_calls - 2*min_calls)*fraction_l*weight_l
+       /(fraction_l*weight_l + (1.0 - fraction_l)*weight_r);
+    calls_r = calls - pre_calls - calls_l;
+
+    gsl_vector_free(fmin_r);
+    gsl_vector_free(fmin_l);
+    xl_tmp = fmax_r; /* reuse vectors */
+    xu_tmp = fmax_l;
+    for (i = 0; i < num_dim; i++) {
+      xl_tmp->data[i] = xl[i];
+      xu_tmp->data[i] = xu[i];
+    }
+
+    xu_tmp->data[i_bisect] = x_mid->data[i_bisect];
+    status = gsl_monte_miser(r, func, xl_tmp->data, xu_tmp->data, 
 		   num_dim, calls_l, &res_l, &err_l);
-    xl_temp->data[j_bisect] = rmid->data[j_bisect];
-    xh_temp->data[j_bisect] = xh[j_bisect];
-    status = gsl_monte_miser(r, func, xl_temp->data, xh_temp->data, 
+    xl_tmp->data[i_bisect] = x_mid->data[i_bisect];
+    xu_tmp->data[i_bisect] = xu[i_bisect];
+    status = gsl_monte_miser(r, func, xl_tmp->data, xu_tmp->data, 
 		   num_dim, calls_r, res, err);
 
-    *res = res_l + (*res);
+    *res += res_l;
     *err = sqrt( SQR(err_l) + SQR(*err) );   
 
-    gsl_vector_free(xl_temp);
-    gsl_vector_free(xh_temp);
-    gsl_vector_free(rmid);
+    gsl_vector_free(xl_tmp);
+    gsl_vector_free(xu_tmp);
+    gsl_vector_free(x_mid);
   }
 
-  gsl_vector_free(pt);
+  gsl_vector_free(x);
 
   return status;
 }
