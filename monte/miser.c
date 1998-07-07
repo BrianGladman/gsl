26fd01f05542344bff05fd830c5fcf81ed7fc3e2
booth <booth>
1998-07-07 20:24:58 +0000

    Allow different methods of estimating the variances.  Start to validate the input and warn about exceptional cases.  This is actually interesting.

--- monte/miser.c
+++ monte/miser.c
@@ -16,6 +16,7 @@
 /* gsl headers */
 #include <gsl_math.h>
 #include <gsl_vector.h>
+#include <gsl_vector_int.h>
 #include <gsl_errno.h>
 #include <gsl_rng.h>
 
@@ -30,10 +31,13 @@
 /* these should be in the state structure */
 size_t min_calls = 15;
 size_t min_calls_per_bisection = 60;
-double precond_frac =  0.1;
+double estimate_frac =  0.1;
 double ALPHA = 2.0/3.0; 
 double dither;
 
+enum {ESTIMATE_STYLE_NR = -1,  ESTIMATE_STYLE_MC = 1};
+
+int estimate_style = -1;
 
 int gsl_monte_miser(const gsl_rng * r, 
 		    double (*func)(double []), double xl[], double xu[], 
@@ -43,18 +47,22 @@ int gsl_monte_miser(const gsl_rng * r,
 
   gsl_vector *xl_tmp, *xu_tmp;
 
-  size_t n, pre_calls, calls_l, calls_r;
+  size_t n, estimate_calls, calls_l, calls_r;
   size_t i;
   int i_bisect;
   double res_l, err_l;
   double fraction_l;
   double f;
   double xbi_l, xbi_m, xbi_r, s;
-  double sigma_l, sigma_r;
+
   double weight_l, weight_r;
-  double sum, sum_bisect;
+  double var, best_var;
+  double vol;
 
   gsl_vector *fmax_l, *fmax_r, *fmin_l, *fmin_r;
+  gsl_vector *sigma_l, *sigma_r;
+  gsl_vector *sum_l, *sum_r, *sum2_l, *sum2_r;
+  gsl_vector_int *hits_l, *hits_r;
   gsl_vector *x, *x_mid;
 
   x = gsl_vector_alloc(num_dim);
@@ -63,22 +71,26 @@ int gsl_monte_miser(const gsl_rng * r,
     status = gsl_monte_plain(r, func, xl, xu, num_dim, calls, res, err);
   }
   else {
-    pre_calls = myMAX((size_t)(calls*precond_frac), min_calls);
+    /* FIXME: This is bad when the estimate_calls come out less than
+       or near to num_dim, because then we will get subplanes.  This
+       is also an issue for min_calls. 
+    */
+    estimate_calls = myMAX(min_calls, (size_t)(calls*estimate_frac) );
+    if (estimate_calls >= num_dim) {
+      GSL_WARNING("estimate calls is close to nun_dim!", GSL_ESANITY);
+    }
 
     x_mid = gsl_vector_alloc(num_dim);
-    fmax_l = gsl_vector_alloc(num_dim);
-    fmax_r = gsl_vector_alloc(num_dim);
-    fmin_l = gsl_vector_alloc(num_dim);
-    fmin_r = gsl_vector_alloc(num_dim);
 
+    sigma_l = gsl_vector_alloc(num_dim);
+    sigma_r = gsl_vector_alloc(num_dim);
+
+    vol = 1;
     for (i = 0; i < num_dim; i++) {
-
       /* flip a coin to bisect the integration region with some fuzz */
       s = (0.5 - gsl_rng_uniform(r) >= 0.0 ? dither : -dither ); 
       x_mid->data[i] = (0.5 + s)*xl[i] + (0.5 - s)*xu[i];
-
-      fmin_l->data[i] = fmin_r->data[i] = DBL_MAX;
-      fmax_l->data[i] = fmax_r->data[i] = -DBL_MAX;
+      vol *= xu[i] - xl[i];
     }
     
     /* 
@@ -88,40 +100,136 @@ int gsl_monte_miser(const gsl_rng * r,
        the variances by finding the min and max function values 
        for each half-region for each bisection.
     */
-    for (n = 1; n <= pre_calls; n++) {
-      for (i = 0; i < num_dim; i++)
-	x->data[i] = xl[i] + (xu[i] - xl[i])*gsl_rng_uniform(r);
-      f = (*func)(x->data);
+    if (estimate_style == ESTIMATE_STYLE_NR ) {
+      /* NR way */
+      fmax_l = gsl_vector_alloc(num_dim);
+      fmax_r = gsl_vector_alloc(num_dim);
+      fmin_l = gsl_vector_alloc(num_dim);
+      fmin_r = gsl_vector_alloc(num_dim);
+
       for (i = 0; i < num_dim; i++) {
-	if (x->data[i] <= x_mid->data[i]) {
-	  fmin_l->data[i] = myMIN(fmin_l->data[i], f);
-	  fmax_l->data[i] = myMAX(fmax_l->data[i], f);
-	}
-	else {
-	  fmin_r->data[i] = myMIN(fmin_r->data[i], f);
-	  fmax_r->data[i] = myMAX(fmax_r->data[i], f);
+	fmin_l->data[i] = fmin_r->data[i] = DBL_MAX;
+	fmax_l->data[i] = fmax_r->data[i] = -DBL_MAX;
+      }
+
+      for (n = 1; n <= estimate_calls; n++) {
+	for (i = 0; i < num_dim; i++)
+	  x->data[i] = xl[i] + (xu[i] - xl[i])*gsl_rng_uniform(r);
+	f = (*func)(x->data);
+	for (i = 0; i < num_dim; i++) {
+	  if (x->data[i] <= x_mid->data[i]) {
+	    fmin_l->data[i] = myMIN(fmin_l->data[i], f);
+	    fmax_l->data[i] = myMAX(fmax_l->data[i], f);
+	  }
+	  else {
+	    fmin_r->data[i] = myMIN(fmin_r->data[i], f);
+	    fmax_r->data[i] = myMAX(fmax_r->data[i], f);
+	  }
 	}
       }
+      for (i = 0; i < num_dim; i++) {
+	if (fmax_l->data[i] >= fmin_l->data[i] && 
+	    fmax_r->data[i] >= fmin_r->data[i]) {
+	  sigma_l->data[i] = 
+	    myMAX(GSL_MACH_EPS, fmax_l->data[i] - fmin_l->data[i]);
+	  sigma_r->data[i] = 
+	    myMAX(GSL_MACH_EPS, fmax_r->data[i] - fmin_r->data[i]);
+	}
+	else {
+	  /* must be that no points landed in one of the half-regions */
+	  sigma_l->data[i] = sigma_r->data[i] = -1;
+	}
+      }
+      gsl_vector_free(fmin_r);
+      gsl_vector_free(fmin_l);
+      gsl_vector_free(fmax_r);
+      gsl_vector_free(fmax_l);
     }
-    /* Now find direction with the largest variance */
-    sum = 0;
-    sum_bisect = DBL_MAX;
+    else if (estimate_style == ESTIMATE_STYLE_MC) {
+      /* assert estimate_style = 1 */
+
+      hits_l = gsl_vector_int_alloc(num_dim);
+      hits_r = gsl_vector_int_alloc(num_dim);
+      sum_l = gsl_vector_alloc(num_dim);
+      sum2_l = gsl_vector_alloc(num_dim);
+      sum_r = gsl_vector_alloc(num_dim);
+      sum2_r = gsl_vector_alloc(num_dim);
+
+      for (i = 0; i < num_dim; i++) {
+	hits_l->data[i] = hits_r->data[i] = 0;
+	sum_l->data[i] = sum_r->data[i] = 0.0;
+	sum2_l->data[i] = sum2_r->data[i] = 0.0;
+	sigma_l->data[i] = sigma_r->data[i] = -1;
+      }
+
+      for (n = 1; n <= estimate_calls; n++) {
+	for (i = 0; i < num_dim; i++)
+	  x->data[i] = xl[i] + (xu[i] - xl[i])*gsl_rng_uniform(r);
+	f = (*func)(x->data);
+	for (i = 0; i < num_dim; i++) {
+	  if (x->data[i] <= x_mid->data[i]) {
+	    sum_l->data[i] += f;
+	    sum2_l->data[i] = f*f;
+	    hits_l->data[i]++;
+	  }
+	  else {
+	    sum_r->data[i] += f;
+	    sum2_r->data[i] = f*f;
+	    hits_r->data[i]++;
+	  }
+	}
+      }
+      for (i = 0; i < num_dim; i++) {
+	fraction_l = (x_mid->data[i] - xl[i])/(xu[i] - xl[i]);
+	if (hits_l->data[i] > 0 ) {
+	  sum2_l->data[i] /= SQR(hits_l->data[i]);
+	  sum_l->data[i] /= hits_l->data[i];
+	  sigma_l->data[i] = sqrt(sum2_l->data[i] - SQR(sum_l->data[i]));
+	  sigma_l->data[i] *= fraction_l*vol;
+	}
+	if (hits_l->data[i] > 0 ) {
+	  sum2_r->data[i] /= SQR(hits_r->data[i]);
+	  sum_r->data[i] /= hits_r->data[i];
+	  sigma_r->data[i] = sqrt(sum2_r->data[i] - SQR(sum_r->data[i]));
+	  sigma_r->data[i] *= (1 - fraction_l)*vol;
+	}
+      }
+
+      gsl_vector_int_free(hits_l);
+      gsl_vector_int_free(hits_r);
+      gsl_vector_free(sum_l);
+      gsl_vector_free(sum2_l);
+      gsl_vector_free(sum_r);
+      gsl_vector_free(sum2_r);
+    }
+    else {
+      GSL_ERROR("estimate_style has two possibilities!", GSL_ESANITY);
+    }
+
+    /* Now find direction with the smallest total "variance" */
+    var = 0;
+    best_var = DBL_MAX;
     i_bisect = -1;
     weight_l = weight_r = 1.0;
 
-    for (i = 0; i < num_dim;i++) {
-      if (fmax_l->data[i] > fmin_l->data[i] && 
-	  fmax_r->data[i] > fmin_r->data[i]) {
-	sigma_l = myMAX(GSL_MACH_EPS, fmax_l->data[i] - fmin_l->data[i]);
-	sigma_r = myMAX(GSL_MACH_EPS, fmax_r->data[i] - fmin_r->data[i]);
-	sum = pow(sigma_l, ALPHA) + pow(sigma_r, ALPHA);
-
-	if (sum <= sum_bisect) {
-	  sum_bisect = sum;
+    for (i = 0; i < num_dim; i++) {
+      if (sigma_l->data[i] >= 0 && sigma_r->data[i] >= 0) {
+	/* estimates are okay */
+	var = pow(sigma_l->data[i], ALPHA) + pow(sigma_r->data[i], ALPHA);
+	if (var <= best_var) {
+	  best_var = var;
 	  i_bisect = i;
-	  weight_l = pow(sigma_l, ALPHA);
-	  weight_r = pow(sigma_r, ALPHA);
+	  weight_l = pow(sigma_l->data[i], ALPHA);
+	  weight_r = pow(sigma_r->data[i], ALPHA);
 	}
+      }
+      else {
+	if (sigma_l->data[i] < 0) 
+	  /* FIXME: Get a proper error code here */
+	  GSL_WARNING("no points in left half-space!?", GSL_ESANITY);
+	if (sigma_r->data[i] < 0) 
+	  /* FIXME: Get a proper error code here */
+	  GSL_WARNING("no points in right half-space!?", GSL_ESANITY);
       }
     }
 
@@ -139,14 +247,12 @@ int gsl_monte_miser(const gsl_rng * r,
     fraction_l = fabs((xbi_m - xbi_l)/(xbi_r - xbi_l));
     calls_l = min_calls;
     calls_l += (size_t)
-      (calls - pre_calls - 2*min_calls)*fraction_l*weight_l
+      (calls - estimate_calls - 2*min_calls)*fraction_l*weight_l
        /(fraction_l*weight_l + (1.0 - fraction_l)*weight_r);
-    calls_r = calls - pre_calls - calls_l;
+    calls_r = calls - estimate_calls - calls_l;
 
-    gsl_vector_free(fmin_r);
-    gsl_vector_free(fmin_l);
-    xl_tmp = fmax_r; /* reuse vectors */
-    xu_tmp = fmax_l;
+    xl_tmp = sigma_l; /* reuse vectors */
+    xu_tmp = sigma_r;
     for (i = 0; i < num_dim; i++) {
       xl_tmp->data[i] = xl[i];
       xu_tmp->data[i] = xu[i];
