f7bd0a472ec825f8448a65dfdb3f533441583a26
bjg <bjg>
1998-07-11 22:42:54 +0000

    incorporated own gaussian generator

--- randist/lognormal.c
+++ randist/lognormal.c
@@ -2,16 +2,28 @@
 #include <gsl_rng.h>
 #include <gsl_randist.h>
 
-#include "gaussian.c"
+/* Lognormal random numbers are the exponentials of gaussian random numbers */
 
 double
 gsl_ran_lognormal (const gsl_rng * r)
 {
-  double x = gaussian (r);  /* First compute a gaussian */
+  double u, v, r2, normal, z;
 
-  /* Lognormal numbers are the exponentials of gaussian random numbers */
+  do
+    {
+      /* choose x,y in uniform square (-1,-1) to (+1,+1) */
 
-  double z = exp (x) ;
+      u = -1 + 2 * gsl_rng_uniform (r);
+      v = -1 + 2 * gsl_rng_uniform (r);
+
+      /* see if it is in the unit circle */
+      r2 = u * u + v * v;
+    }
+  while (r2 > 1.0 || r2 == 0);
+
+  normal = u * sqrt (-2.0 * log (r2) / r2) ;
+
+  z = exp(normal) ;
 
   return z ;
 }
