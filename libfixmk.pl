7a1e67f3d66d66e3ee5aca3ed35d080788d9f2b6
bjg <bjg>
1998-07-31 00:15:33 +0000

    trying out a new makefile scheme using a script to generate dependencies

---
+++ libfixmk.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+while (<>) {
+    $line = "" ;
+
+    if (s/\s+\\$/ /) {
+	chomp;
+	$line = $_ ;
+	while (<>) {
+	    chomp ;
+	    if (s/\s+\\$/ /) {
+		$line .= $_ ;
+	    } else {
+		$line .= $_ ;
+		last ;
+	    }
+	}
+    } else {
+	$line = $_ ;
+    }
+
+    if ($line =~ /SUBDIRS\s*=\s*(.*)/) {
+	@dirs = split(' ',$1) ;
+	for (@dirs) {
+	    push (@ARGV, "$_/Makefile") ;
+	}
+    }
+
+    if ($line =~ /(libgsl\w+)_a_OBJECTS\s*=\s*(.*)/) {
+	$lib = $1 ;
+	($dir) = split('/', $ARGV) ;
+	@files = split(' ',$2) ;
+	@files = map("$dir/$_", @files) ;
+	$cmd = "\t" . "\$(AR) q libgsl.a " . join(" ",@files) ;
+	push(@cmds,$cmd) ;
+	push(@deps,"$dir/$lib.a") ;
+    }
+}
+
+print "libgsl.a: ", join(" ",@deps), "\n" ;
+print "\t", "\@rm -f libgsl.a\n" ;
+print join("\n", @cmds) ;
+print "\n" ;
+print "\t", "\$(RANLIB) libgsl.a\n" ;
+
