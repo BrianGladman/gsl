f34c5d52e07eddec5c9a04e8c7eb6d95ee25cf55
jungman <jungman>
1998-08-01 02:05:10 +0000

    the dust blows forward and the dust blows back...

--- blas/blas_raw_native_cm.c
+++ blas/blas_raw_native_cm.c
@@ -9,18 +9,6 @@
  * storage scheme.
  */
 #include <math.h>
+#include "raw_complex.h"
 #include "gsl_blas_raw_cm.h"
-
-
-
-/* ===========================================================================
- * Generate the col-major versions of the level 2 and level 3 operations.
- * ===========================================================================
- */
-
-#define  MACCESS(s, i, j)  ((s)*(j) + (i))
-#define  FUNC(f) f ## _ ## cm
-#include "blas_raw_native_L23_source.c"
-#undef   FUNC
-#undef   MACCESS
 
