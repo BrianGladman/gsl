4504ee98aff833bfc73e979e7ad692bb5497b59a
bjg <bjg>
1998-08-05 03:41:01 +0000

    added scale parameter to logistic, weibull and pareto

--- randist/gsl_randist.h
+++ randist/gsl_randist.h
@@ -57,8 +57,8 @@ double gsl_ran_gumbel1_pdf (double x, double a, double b);
 double gsl_ran_gumbel2 (const gsl_rng * r, double a, double b);
 double gsl_ran_gumbel2_pdf (double x, double a, double b);
 
-double gsl_ran_logistic (const gsl_rng * r);
-double gsl_ran_logistic_pdf (double x);
+double gsl_ran_logistic (const gsl_rng * r, double mu);
+double gsl_ran_logistic_pdf (double x, double mu);
 
 double gsl_ran_lognormal (const gsl_rng * r);
 double gsl_ran_lognormal_pdf (double x);
@@ -69,8 +69,8 @@ double gsl_ran_negative_binomial_pdf (unsigned int n, double p, double t);
 unsigned int gsl_ran_pascal (const gsl_rng * r, double p, unsigned int n);
 double gsl_ran_pascal_pdf (unsigned int k, double p, unsigned int n);
 
-double gsl_ran_pareto (const gsl_rng * r, double a);
-double gsl_ran_pareto_pdf (double x, double a);
+double gsl_ran_pareto (const gsl_rng * r, double mu, double a);
+double gsl_ran_pareto_pdf (double x, double mu, double a);
 
 unsigned int gsl_ran_poisson (const gsl_rng * r, double mu);
 void gsl_ran_poisson_array (const gsl_rng * r, size_t n, unsigned int array[],
@@ -83,8 +83,10 @@ double gsl_ran_tdist_pdf (double x, double nu);
 double gsl_ran_laplace (const gsl_rng * r, double mu);
 double gsl_ran_laplace_pdf (double x, double mu);
 
-double gsl_ran_weibull (const gsl_rng * r, double a);
-double gsl_ran_weibull_pdf (double x, double a);
+double gsl_ran_levy (const gsl_rng * r, double mu, double a);
+
+double gsl_ran_weibull (const gsl_rng * r, double mu, double a);
+double gsl_ran_weibull_pdf (double x, double mu, double a);
 
 void gsl_ran_dir_2d (const gsl_rng * r, double * x, double * y);
 void gsl_ran_dir_3d (const gsl_rng * r, double * x, double * y, double * z);
