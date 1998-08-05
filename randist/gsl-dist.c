413c2f25d0c26a5fe8231409797f618ec62e584d
bjg <bjg>
1998-08-05 03:39:28 +0000

    a program for generating samples from the distributions

--- randist/gsl-dist.c
+++ randist/gsl-dist.c
@@ -8,45 +8,230 @@
 #include <gsl_rng.h>
 #include <gsl_test.h>
 
-enum { EXPONENTIAL, CAUCHY, GAUSS } type;
+void error (const char * s);
+
 
 int
 main (int argc, char *argv[])
 {
   size_t i;
   size_t n = 0;
-  double mu = 1;
-  double a = 1;
+  double mu, nu, nu1, nu2, sigma, a, b, p;
+  unsigned int N, t, n1, n2 ;
   const char * name ;
-  double x;
   gsl_rng * r ;
 
   gsl_rng_env_setup() ;
 
   r = gsl_rng_alloc(gsl_rng_default) ;
 
-  if (argc == 3) 
+  if (argc < 3) 
     {
-      argc = 3; 
+      printf (
+"Usage: gsl-randist n DIST param1 param2 ...\n"
+"Generates n samples from the distribution DIST with parameters param1,\n"
+"param2, etc. Valid distributions are,\n"
+"\n"
+"  exponential\n"
+"  levy\n") ;
+      exit (0);
     }
 
-  name = argv[1] ;
-  n = strtol (argv[2], NULL, 0);
+  
+  argv++ ; n = atol (argv[0]); argc-- ;
+  argv++ ; name = argv[0] ; argc-- ; argc-- ;
 
 #define NAME(x) !strcmp(name,(x))
-    
-  if (NAME("exponential"))
+#define OUTPUT(x) for (i = 0; i < n; i++) { printf("%g\n", (x)) ; }
+#define INT_OUTPUT(x) for (i = 0; i < n; i++) { printf("%d\n", (x)) ; }
+#define ARGS(x,y) if (argc != x) error(y) ;
+#define DBL_ARG(x) if (argc) { x=atof((++argv)[0]);argc--;} else {error( #x);};
+#define INT_ARG(x) if (argc) { x=atoi((++argv)[0]);argc--;} else {error( #x);};
+
+  if (NAME("bernoulli"))
     {
-      for (i = 0; i < n; i++)
-	printf("%g\n",gsl_ran_exponential (r, mu));
+      ARGS(1, "p = probability of success");
+      DBL_ARG(p)
+      INT_OUTPUT(gsl_ran_bernoulli (r, p));
+    }
+  else if (NAME("beta"))
+    {
+      ARGS(2, "a,b = shape parameters");
+      DBL_ARG(a)
+      DBL_ARG(b)
+      OUTPUT(gsl_ran_beta (r, a, b));
+    }
+  else if (NAME("binomial"))
+    {
+      ARGS(2, "p = probability, N = number of trials");
+      DBL_ARG(p)
+      INT_ARG(N)
+      INT_OUTPUT(gsl_ran_binomial (r, p, N));
+    }
+  else if (NAME("cauchy"))
+    {
+      ARGS(1, "mu = scale parameter");
+      DBL_ARG(mu)
+      OUTPUT(gsl_ran_cauchy (r, mu));
+    }
+  else if (NAME("chisq"))
+    {
+      ARGS(1, "nu = degrees of freedom");
+      DBL_ARG(nu)
+      OUTPUT(gsl_ran_chisq (r, nu));
+    }
+  else if (NAME("erlang"))
+    {
+      ARGS(2, "a = scale parameter, b = order");
+      DBL_ARG(a)
+      DBL_ARG(b)
+      OUTPUT(gsl_ran_erlang (r, a, b));
+    }
+  else if (NAME("exponential"))
+    {
+      ARGS(1, "mu = mean value");
+      DBL_ARG(mu) ;
+      OUTPUT(gsl_ran_exponential (r, mu));
+    }
+  else if (NAME("exppow"))
+    {
+      ARGS(2, "mu = scale parameter, a = power (1=exponential, 2=gaussian)");
+      DBL_ARG(mu) ;
+      DBL_ARG(a) ;
+      OUTPUT(gsl_ran_exppow (r, mu, a));
+    }
+  else if (NAME("fdist"))
+    {
+      ARGS(2, "nu1, nu2 = degrees of freedom parameters");
+      DBL_ARG(nu1) ;
+      DBL_ARG(nu2) ;
+      OUTPUT(gsl_ran_fdist (r, nu1, nu2));
+    }
+  else if (NAME("flat"))
+    {
+      ARGS(2, "a = lower limit, b = upper limit");
+      DBL_ARG(a) ;
+      DBL_ARG(b) ;
+      OUTPUT(gsl_ran_flat (r, a, b));
+    }
+  else if (NAME("gamma"))
+    {
+      ARGS(1, "a = order");
+      DBL_ARG(a) ;
+      OUTPUT(gsl_ran_gamma (r, a));
+    }
+  else if (NAME("gaussian"))
+    {
+      ARGS(1, "sigma = standard deviation");
+      DBL_ARG(sigma) ;
+      OUTPUT(gsl_ran_gaussian (r, sigma));
+    }
+  else if (NAME("geometric"))
+    {
+      ARGS(1, "p = bernoulli trial probability of success");
+      DBL_ARG(p) ;
+      INT_OUTPUT(gsl_ran_geometric (r, p));
+    }
+  else if (NAME("gumbel1"))
+    {
+      ARGS(2, "a = order, b = scale parameter");
+      DBL_ARG(a) ;
+      DBL_ARG(b) ;
+      OUTPUT(gsl_ran_gumbel1 (r, a, b));
+    }
+  else if (NAME("gumbel2"))
+    {
+      ARGS(2, "a = order, b = scale parameter");
+      DBL_ARG(a) ;
+      DBL_ARG(b) ;
+      OUTPUT(gsl_ran_gumbel2 (r, a, b));
+    }
+  else if (NAME("hypergeometric"))
+    {
+      ARGS(3, "n1 = tagged population, n2 = untagged population, t = number of trials");
+      INT_ARG(n1) ;
+      INT_ARG(n2) ;
+      INT_ARG(t) ;
+      INT_OUTPUT(gsl_ran_hypergeometric (r, n1, n2, t));
+    }
+  else if (NAME("laplace"))
+    {
+      ARGS(1, "mu = scale parameter");
+      DBL_ARG(mu) ;
+      OUTPUT(gsl_ran_laplace (r, mu));
     }
   else if (NAME("levy"))
     {
-      for (i = 0; i < n; i++)
-	printf("%g\n",gsl_ran_levy (r, mu, a));
+      ARGS(2, "mu = scale parameter, a = power (1=cauchy, 2=gaussian)");
+      DBL_ARG(mu) ;
+      DBL_ARG(a) ;
+      OUTPUT(gsl_ran_levy (r, mu, a));
+    }
+  else if (NAME("logistic"))
+    {
+      ARGS(1, "mu = scale parameter");
+      DBL_ARG(mu) ;
+      OUTPUT(gsl_ran_logistic (r, mu));
+    }
+  else if (NAME("lognormal"))
+    {
+      ARGS(2, "mu = scale parameter, a = power (1=exponential, 2=gaussian)");
+      DBL_ARG(mu) ;
+      DBL_ARG(a) ;
+      OUTPUT(gsl_ran_exppow (r, mu, a));
+    }
+  else if (NAME("negative-binomial"))
+    {
+      ARGS(2, "p = probability, a = order");
+      DBL_ARG(p) ;
+      DBL_ARG(a) ;
+      INT_OUTPUT(gsl_ran_negative_binomial (r, p, a));
+    }
+  else if (NAME("pareto"))
+    {
+      ARGS(2, "mu = scale parameter, a = power");
+      DBL_ARG(mu) ;
+      DBL_ARG(a) ;
+      OUTPUT(gsl_ran_pareto (r, mu, a));
+    }
+  else if (NAME("pascal"))
+    {
+      ARGS(2, "p = probability, n = order (integer)");
+      DBL_ARG(mu) ;
+      INT_ARG(N) ;
+      INT_OUTPUT(gsl_ran_pascal (r, mu, N));
+    }
+  else if (NAME("poisson"))
+    {
+      ARGS(1, "mu = scale parameter");
+      DBL_ARG(mu) ;
+      INT_OUTPUT(gsl_ran_poisson (r, mu));
+    }
+  else if (NAME("tdist"))
+    {
+      ARGS(1, "nu = degrees of freedom");
+      DBL_ARG(nu) ;
+      OUTPUT(gsl_ran_tdist (r, nu));
+    }
+  else if (NAME("weibull"))
+    {
+      ARGS(2, "mu = scale parameter, a = exponent");
+      DBL_ARG(mu) ;
+      DBL_ARG(a) ;
+      OUTPUT(gsl_ran_weibull (r, mu, a));
+    }
+  else
+    {
+      fprintf(stderr,"Error: unrecognized distribution: %s\n", name) ;
     }
 
   return 0 ;
 }
 
 
+void
+error (const char * s)
+{
+  fprintf(stderr, "Error: arguments should be %s\n",s) ;
+  exit (EXIT_FAILURE) ;
+}
