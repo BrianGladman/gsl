4cd9a83f18584541f0d416b5de52e35cacab0280
bjg <bjg>
1998-08-05 21:29:20 +0000

    removed duplicate headers

--- vector/vector.c
+++ vector/vector.c
@@ -1,12 +1,8 @@
-
 #include <config.h>
 #include <gsl_vector.h>
 
 /* turn off range checking at runtime if zero */
 int gsl_check_range = 1;
-
-#include <config.h>
-#include <gsl_vector.h>
 
 #define BASE_GSL_COMPLEX_LONG
 #include "templates_on.h"
