8d5deeb80c832faa7e53ea46c02d02d1d9530e21
bjg <bjg>
1998-08-21 23:37:21 +0000

    made return type int

--- ieee-utils/fp-unknown.c
+++ ieee-utils/fp-unknown.c
@@ -2,12 +2,10 @@
 #include <gsl_ieee_utils.h>
 #include <gsl_errno.h>
 
-void
+int
 gsl_ieee_set_mode (int precision, int rounding, int exception_mask)
 {
-  
-  GSL_ERROR ("the IEEE interface for this platform could not be determined "
-	     "at configure time\n",
-	     GSL_EUNSUP) ;
-  
+  GSL_ERROR (
+"the IEEE interface for this platform is unsupported or could not be "
+"determined at configure time\n", GSL_EUNSUP) ;
 }
