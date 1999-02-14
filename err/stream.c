cd619cbabcd5ebd13e2abc76196c73c9c6ec0eb7
rosalia <rosalia>
1999-02-14 17:34:46 +0000

    1999-02-06  Mark Galassi  <rosalia@cygnus.com> 	* NEWS: udpated in occasion of the imminent 0.4.1 release. 1999-01-03  Mark Galassi  <rosalia@cygnus.com> 	* Makefile.am, autogen.sh: improved autogen.sh, based on the gtk+ 	autogen.sh.  Added it to Makefile.am's EXTRA_DIST list.

--- err/stream.c
+++ err/stream.c
@@ -6,13 +6,17 @@
 #include <gsl_errno.h>
 #include <gsl_message.h>
 
-FILE * gsl_stream = stderr ;
+FILE * gsl_stream = NULL ;
 gsl_stream_handler_t * gsl_stream_handler = NULL;
 
 void
 gsl_stream_printf (const char *label, const char *file, int line, 
 		   const char *reason)
 {
+  if (gsl_stream == NULL)
+    {
+      gsl_stream = stderr;
+    }
   if (gsl_stream_handler)
     {
       (*gsl_stream_handler) (label, file, line, reason);
@@ -33,7 +37,11 @@ gsl_set_stream_handler (gsl_stream_handler_t * new_handler)
 FILE *
 gsl_set_stream (FILE * new_stream)
 {
-  FILE * previous_stream = gsl_stream;
+  FILE * previous_stream;
+  if (gsl_stream == NULL) {
+    gsl_stream = stderr;
+  }
+  previous_stream = gsl_stream;
   gsl_stream = new_stream;
   return previous_stream;
 }
