2e1b60717f568e371ef8ddec681e5f2dd8fd2efc
bjg <bjg>
2000-02-26 21:17:09 +0000

    puts the version number into the known problems file

--- mkknownproblems.sh
+++ mkknownproblems.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 make clean
-make -k CFLAGS="-g -O2 -Wall" 2>&1 | tee typescript.make
+echo VERSION: `gsl-config --version` > typescript.make
+make -k CFLAGS="-g -O2 -Wall" 2>&1 | tee -a typescript.make
 make -k check CFLAGS="-g -O2 -Wall" 2>&1 | tee -a typescript.make
 ./knownproblems.pl < typescript.make > KNOWN-PROBLEMS
