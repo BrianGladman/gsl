5bb146e4468cf6f15c422a1501550bfcf44c36e1
bjg <bjg>
2000-03-06 20:49:08 +0000

    added support for versioning based on release numbers

--- version.c
+++ version.c
@@ -1,3 +1,7 @@
 #include <config.h>
+#include <gsl_version.h>
 
-static const char *version = PACKAGE "-" VERSION;
+const unsigned int gsl_major_version = GSL_MAJOR_VERSION;
+const unsigned int gsl_minor_version = GSL_MINOR_VERSION;
+const unsigned int gsl_micro_version = GSL_MICRO_VERSION;
+
