8fbbe1c5b33bcbfa045d716800b044c955ac935d
bjg <bjg>
2000-03-15 12:02:08 +0000

    added a directory for complex number support, complex/

---
+++ complex/gsl_complex.h
@@ -0,0 +1,84 @@
+#ifndef __GSL_COMPLEX_H__
+#define __GSL_COMPLEX_H__
+
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+# define __BEGIN_DECLS extern "C" {
+# define __END_DECLS }
+#else
+# define __BEGIN_DECLS /* empty */
+# define __END_DECLS /* empty */
+#endif
+
+__BEGIN_DECLS
+
+
+/* two consecutive built-in types as a complex number */
+typedef double *       gsl_complex_packed ;
+typedef float *        gsl_complex_packed_float  ;
+typedef long double *  gsl_complex_packed_long_double ;
+
+typedef const double *       gsl_const_complex_packed ;
+typedef const float *        gsl_const_complex_packed_float  ;
+typedef const long double *  gsl_const_complex_packed_long_double ;
+
+
+/* 2N consecutive built-in types as N complex numbers */
+typedef double *       gsl_complex_packed_array ;
+typedef float *        gsl_complex_packed_array_float  ;
+typedef long double *  gsl_complex_packed_array_long_double ;
+
+typedef const double *       gsl_const_complex_packed_array ;
+typedef const float *        gsl_const_complex_packed_array_float  ;
+typedef const long double *  gsl_const_complex_packed_array_long_double ;
+
+
+/* Yes... this seems weird. Trust us. The point is just that
+   sometimes you want to make it obvious that something is
+   an output value. The fact that it lacks a 'const' may not
+   be enough of a clue for people in some contexts.
+ */
+typedef double *       gsl_complex_packed_ptr ;
+typedef float *        gsl_complex_packed_float_ptr  ;
+typedef long double *  gsl_complex_packed_long_double_ptr ;
+
+typedef const double *       gsl_const_complex_packed_ptr ;
+typedef const float *        gsl_const_complex_packed_float_ptr  ;
+typedef const long double *  gsl_const_complex_packed_long_double_ptr ;
+
+
+typedef struct
+  {
+    long double dat[2];
+  }
+gsl_complex_long_double;
+
+typedef struct
+  {
+    double dat[2];
+  }
+gsl_complex;
+
+typedef struct
+  {
+    float dat[2];
+  }
+gsl_complex_float;
+
+#define GSL_REAL(z)     ((z).dat[0])
+#define GSL_IMAG(z)     ((z).dat[1])
+#define GSL_COMPLEX_P_REAL(zp)  ((zp)->dat[0])
+#define GSL_COMPLEX_P_IMAG(zp)  ((zp)->dat[1])
+#define GSL_COMPLEX_EQ(z1,z2) ((z1).dat[0] == (z2).dat[0] && \
+			       ((z1).dat[1] == (z2).dat[1]))
+
+#define GSL_SET_COMPLEX(zp,x,y) do {(zp)->dat[0]=(x); (zp)->dat[1]=(y);} while(0)
+#define GSL_SET_REAL(zp,x) do {(zp)->dat[0]=(x);} while(0)
+#define GSL_SET_IMAG(zp,y) do {(zp)->dat[1]=(y);} while(0)
+
+#define GSL_SET_COMPLEX_PACKED(zp,n,x,y) do {*((zp)+2*(n))=(x); *((zp)+(2*(n)+1))=(y);} while(0)
+
+__END_DECLS
+
+#endif /* __GSL_COMPLEX_H__ */
