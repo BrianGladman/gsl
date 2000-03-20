3544639fd7afebfc53ae022093ecb7a48613bd81
bjg <bjg>
2000-03-20 21:03:41 +0000

    removed extraneous headers

--- ieee-utils/ieee_utils.h
+++ ieee-utils/ieee_utils.h
@@ -1,8 +1,4 @@
 static int little_endian_p (void) ;
-static int endianness (void) ;
-static int double_endianness (void) ;
-static void setup_dynamic_endianness(int *b0,int *b1,int *b2,int *b3,
-			      int *b4,int *b5,int *b6,int *b7);
 
 static void sprint_nybble(int i, char *s) ;
 static void sprint_byte(int i, char *s) ;
