5172fb17abb657ac78cf36fe79dabb4dda8ebc0c
bjg <bjg>
2000-04-11 21:36:11 +0000

    important script for keeping templated header files up to date.  Edit the float and complex_float (optional) headers and use this script to generate the others with ../mkheaders.pl gsl_prefix

---
+++ mkheaders.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+$dir = $ARGV[0];
+$f = "${dir}_float.h" ;
+$x = "float" ;
+#$suff = ".new";
+
+open(FILE,"<$f") ; @lines = <FILE> ; close(FILE) ;
+
+&substitute("double") ;
+&substitute("long double") ;
+&substitute("int") ;
+&substitute("char") ;
+&substitute("short") ;
+&substitute("long") ;
+&substitute("unsigned int") ;
+&substitute("unsigned char") ;
+&substitute("unsigned short") ;
+&substitute("unsigned long") ;
+
+$f = "${dir}_complex_float.h" ;
+$x = "float" ;
+exit if ! -e $f ;
+open(FILE,"<$f") ; @lines = <FILE> ; close(FILE) ;
+&substitute("double") ;
+&substitute("long double") ;
+
+sub substitute {
+    my ($t) = @_ ;
+    
+    $s = $t ;
+    if ($t eq "double") {
+        $s = "" ;
+        $u = $t;
+    } else {
+        $s =~ s/^unsigned /u/;
+        $s =~ s/ /_/g;
+        $u = $s;
+        $s = "_$s" ;
+    }
+
+    ($new = $f) =~ s/$x/$u/;
+    die if $new eq $f ;
+    @temp = @lines ;
+    print $new , "\n";
+    open (FILE,">$new$suff");
+    for (@temp) {
+        $skip = 1 if /DEFINED FOR FLOATING POINT TYPES ONLY/i && $t !~ /float|double/;
+        $skip = 0, next if /END OF FLOATING POINT TYPES/i && $skip;
+        next if $skip ;
+
+        s/_$x\.h/_$u.h/g;
+        s/_$x/$s/g;
+        s/$x/$t/g;
+        s/\U${x}_H/\U${u}_H/g;
+        s/_\U$x/\U$s/g;
+        print FILE $_ ;
+    }
+    close(FILE);
+}
