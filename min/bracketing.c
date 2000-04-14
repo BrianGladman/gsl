7c48187e97d6db42ba6573c8b0eb179f13c4c288
rossi <rossi>
2000-04-14 17:35:24 +0000

    Fixed a precision related bug by declaring some variable volatile to avoid storage in extended precision fp registers.

--- min/bracketing.c
+++ min/bracketing.c
@@ -12,12 +12,20 @@ gsl_min_find_bracket(gsl_function *f,double *minimum,double * f_minimum,
 		     gsl_interval *x, double * f_lower,double * f_upper,
 		     size_t eval_max)
 {
-  double f_left = *f_lower;
+  /* The three following variables must be declared volatile to avoid storage
+     in extended precision registers available on some architecture. The code
+     relies on the ability to compare double values. As the values will be
+     store in regular memory, the extended precision will then be lost and
+     values that are different in extended precision might have equal
+     representation in double precision. This behavior might break the
+     algorithm. 
+   */
+  volatile double f_left = *f_lower;
+  volatile double f_right = *f_upper;
+  volatile double f_center;
   double x_left = x->lower;
-  double f_right = *f_upper;
   double x_right= x->upper; 
   double x_center;
-  double f_center;
   const double golden = 0.318966;	/* golden = (3 - sqrt(5))/2 */
   size_t nb_eval = 0;
   
@@ -49,6 +57,10 @@ gsl_min_find_bracket(gsl_function *f,double *minimum,double * f_minimum,
 	      *f_lower = f_left;
 	      *f_upper = f_right;
 	      *f_minimum = f_center;
+/*	      gsl_ieee_printf_double (&f_left);
+	      printf(" ");
+	      gsl_ieee_printf_double (&f_center);
+	      printf("\n");*/
 	      return GSL_SUCCESS;
 	    }
 	  else if (f_center > f_right)
