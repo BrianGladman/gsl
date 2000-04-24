9a80a815c228d8ff3ebdb924579d90436074e305
bjg <bjg>
2000-04-24 22:19:05 +0000

    preparing documentation a new release

--- doc/mkdefuns.pl
+++ doc/mkdefuns.pl
@@ -4,7 +4,8 @@ $/=';' ;
 while (<>) {
     s/\/\*/\001/g;
     s/\*\//\002/g;
-    s/\001[^\002]*\002//g;
+    s/\001[^\002]*//g;
+    s/[^\001]*\002//g;
     s/^#.*//mg;
     s/__BEGIN_DECLS//g;
     s/__END_DECLS//g;
@@ -19,10 +20,3 @@ while (<>) {
     print "\@deftypefun $_\n";
     print "\@end deftypefun\n\n" ;
 }
-
-#s/(\w+)(,|\))/\@var{$1}$2/g, s/;$// if /^\@deftypefun/ && !/\@var/;
-#s/\@var\{\*/*\@var\{/g if /^\@deftypefun/;
-
-##!/usr/bin/perl -p 
-##s/^/\@deftypefun / if /^\w+ gsl_/;
-
