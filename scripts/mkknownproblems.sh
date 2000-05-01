1acd633110c0de18c2385659f0af7a54257bcb98
bjg <bjg>
2000-05-01 23:15:11 +0000

    modified to work from scripts subdirectory

--- scripts/mkknownproblems.sh
+++ scripts/mkknownproblems.sh
@@ -3,4 +3,4 @@ make clean
 echo VERSION: `sh ./gsl-config --version` > typescript.make
 make -k CFLAGS="-g -O2 -Wall" 2>&1 | tee -a typescript.make
 make -k check CFLAGS="-g -O2 -Wall" 2>&1 | tee -a typescript.make
-./knownproblems.pl < typescript.make > KNOWN-PROBLEMS
+./scripts/knownproblems.pl < typescript.make > KNOWN-PROBLEMS
