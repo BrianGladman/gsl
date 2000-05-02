8ed14206cb6cc7d8fa81736321fd842792d51e18
bjg <bjg>
2000-05-02 21:28:46 +0000

    made it build in separate directory so that we test relative path for make

--- scripts/mkknownproblems.sh
+++ scripts/mkknownproblems.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+mkdir =gsl.build
+cd =gsl.build
+../configure --disable-shared
 make clean
 echo VERSION: `sh ./gsl-config --version` > typescript.make
-make -k CFLAGS="-g -O2 -Wall" 2>&1 | tee -a typescript.make
-make -k check CFLAGS="-g -O2 -Wall" 2>&1 | tee -a typescript.make
-./scripts/knownproblems.pl < typescript.make > KNOWN-PROBLEMS
+echo running make
+make -k CFLAGS="-g -O2 -Wall" >>typescript.make 2>&1 
+echo running make check
+make -k check CFLAGS="-g -O2 -Wall" >>typescript.make 2>&1 
+../scripts/knownproblems.pl < typescript.make > ../KNOWN-PROBLEMS
