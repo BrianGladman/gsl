e6b3e8467ce0caa5e8ce34fe4b83371ebda74fd3
bjg <bjg>
2000-05-04 20:13:06 +0000

    changed copyright to Fabrice Rossi

--- min/bracketing.c
+++ min/bracketing.c
@@ -1,6 +1,6 @@
 /* min/bracketing.c
  * 
- * Copyright (C) 1996, 1997, 1998, 1999, 2000 Brian Gough
+ * Copyright (C) 1996, 1997, 1998, 1999, 2000 Fabrice Rossi
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
