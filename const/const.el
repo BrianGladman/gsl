aac8ccd04075117749498a24ed7f4b5c6c3d679f
bjg <bjg>
2000-08-16 21:37:55 +0000

    fixed bug in calc unit conversion.

--- const/const.el
+++ const/const.el
@@ -2,31 +2,57 @@
 ;;
 ;; Generate output with
 ;;
-;;   emacs -batch -l test.el -f run 
+;;   emacs -batch -l const.el -f run 
 ;;
 
 (setq calc-display-working-message t) ;; display short working messages
-(calc-eval "")
-(calc-extensions)
 (setq calc-float-format '(sci 20))
+(calc-eval "")
+(load-library "calc/calc-units.el")
+(calc-extensions)
 
 (setq  gsl-constants
   '(("c" "SPEED_OF_LIGHT")
-    ("au" "ASTRONOMICAL_UNIT"))
+    ("au" "ASTRONOMICAL_UNIT")
+    ("Grav" "GRAVITATIONAL_CONSTANT"))
   )
 
-(defun fn (expr name)
-  (let ((value (calc-eval 
-                (math-remove-units 
-                 (math-to-standard-units (calc-eval expr 'raw) nil)))))
-    (princ (format "#define GSL_CONST_%s (%s)\n" name value))
+;;; work around bug in calc 2.02f
+(defun math-extract-units (expr)
+  (if (memq (car-safe expr) '(* /))
+      (cons (car expr)
+	    (mapcar 'math-extract-units (cdr expr)))
+    (if (math-units-in-expr-p expr nil) expr 1))
+)
+
+
+
+;;  (nth 1 (assq 'cgs math-standard-units-systems))
+
+
+
+
+(defun fn (prefix system expr name)
+  (let* ((x (calc-eval expr 'raw))
+         (y (math-to-standard-units x system))
+         (z (math-simplify-units y))
+         (quantity (calc-eval (math-remove-units z)))
+         (units (calc-eval (math-extract-units z)))
+         )
+    (print x)
+    (print y)
+    (print z)
+    (print (math-extract-units z))
+    (print quantity)
+    (print units)
+    (princ (format "#define %s_%s (%s) /* %s */\n" prefix name quantity units))
     )
   )
 
+(setq cgs (nth 1 (assq 'cgs math-standard-units-systems)))
+
+
 (defun run ()
-  (mapcar (lambda (x) (apply 'fn x)) gsl-constants)
+  (mapcar (lambda (x) (apply 'fn "GSL_CONST" cgs x)) gsl-constants)
 )
 
-;;(apply 'fn (nth 1 gsl-constants))
-
-;;(fn "au" "x")
