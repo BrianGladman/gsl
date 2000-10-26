40af1c40d309556ee71073d437b131b83e5259e7
rosalia <rosalia>
2000-10-26 21:37:29 +0000

    2000-10-26  Mark Galassi  <rosalia@lanl.gov>
    	* scripts/mkknownproblems.sh: fixed it so it's slightly better,
    	but it still assumes that you run it out of $(srcdir)/scripts.
    
    	* scripts/knownproblems.pl: put in a more standard path for perl.

--- scripts/mkknownproblems.sh
+++ scripts/mkknownproblems.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 DIR=/tmp/gsl.build
+SRCDIR=`pwd`/..			# assume we build in $(srcdir)/scripts
 echo building in $DIR 
 mkdir $DIR
 cd $DIR
 #../configure --disable-shared
-../configure
+#../configure
+${SRCDIR}/configure
 make clean
 echo VERSION: `sh ./gsl-config --version` > typescript.make
 echo Running make >>typescript.make 2>&1 
@@ -14,4 +16,4 @@ make -k check CFLAGS="-g -O2 -Wall" >>typescript.make 2>&1
 echo Running make check in double-precision >>typescript.make 2>&1 
 export GSL_IEEE_MODE=double-precision,mask-all
 make -k check CFLAGS="-g -O2 -Wall" >>typescript.make 2>&1 
-../scripts/knownproblems.pl < typescript.make > ../KNOWN-PROBLEMS
+${SRCDIR}/scripts/knownproblems.pl < typescript.make > ${SRCDIR}/KNOWN-PROBLEMS
