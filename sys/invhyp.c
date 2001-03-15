ad2c21d2e45ff89ba12e63daae64eba111fdc6b1
bjg <bjg>
2001-03-15 14:17:54 +0000

    cleaned up formatting

--- sys/invhyp.c
+++ sys/invhyp.c
@@ -24,20 +24,21 @@
 
 double gsl_acosh (const double x);
 
-double gsl_acosh (const double x)
+double
+gsl_acosh (const double x)
 {
   if (x > 1.0 / GSL_SQRT_DBL_EPSILON)
     {
-      return log(x) + M_LN2;
+      return log (x) + M_LN2;
     }
   else if (x > 2)
     {
-      return log (2*x - 1/(sqrt(x*x-1) + x));
+      return log (2 * x - 1 / (sqrt (x * x - 1) + x));
     }
   else if (x > 1)
     {
       double t = x - 1;
-      return log1p(t + sqrt(2*t + t*t));
+      return log1p (t + sqrt (2 * t + t * t));
     }
   else if (x == 1)
     {
@@ -51,36 +52,38 @@ double gsl_acosh (const double x)
 
 double gsl_asinh (const double x);
 
-double gsl_asinh (const double x)
+double
+gsl_asinh (const double x)
 {
-  double a = fabs(x);
-  double s = (x < 0) ? -1 : 1 ;
+  double a = fabs (x);
+  double s = (x < 0) ? -1 : 1;
 
   if (a > 1 / GSL_SQRT_DBL_EPSILON)
     {
-      return s * (log(a) + M_LN2);
+      return s * (log (a) + M_LN2);
     }
   else if (a > 2)
     {
-      return s * log (2 * a + 1 / (a + sqrt(a * a + 1)));
+      return s * log (2 * a + 1 / (a + sqrt (a * a + 1)));
     }
   else if (a > GSL_SQRT_DBL_EPSILON)
     {
       double a2 = a * a;
-      return s * log1p (a + a2 / (1 + sqrt(1 + a2)));
+      return s * log1p (a + a2 / (1 + sqrt (1 + a2)));
     }
-  else 
+  else
     {
-      return x ;
+      return x;
     }
 }
 
 double gsl_atanh (const double x);
 
-double gsl_atanh (const double x)
+double
+gsl_atanh (const double x)
 {
-  double a = fabs(x);
-  double s = (x < 0) ? -1 : 1 ;
+  double a = fabs (x);
+  double s = (x < 0) ? -1 : 1;
 
   if (a > 1)
     {
@@ -88,7 +91,7 @@ double gsl_atanh (const double x)
     }
   else if (a == 1)
     {
-      return (x < 0) ? GSL_NEGINF : GSL_POSINF ;
+      return (x < 0) ? GSL_NEGINF : GSL_POSINF;
     }
   else if (a >= 0.5)
     {
@@ -98,9 +101,8 @@ double gsl_atanh (const double x)
     {
       return s * 0.5 * log1p (2 * a + 2 * a * a / (1 - a));
     }
-  else 
+  else
     {
       return x;
     }
 }
-
