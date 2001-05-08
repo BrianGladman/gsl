4f50e5edf202edf9e18e55ff5579595459b4be0b
bjg <bjg>
2001-05-08 21:30:38 +0000

    return nan for GSL_EDOM

--- specfunc/eval.h
+++ specfunc/eval.h
@@ -3,14 +3,18 @@
 #define EVAL_RESULT(fn) \
    gsl_sf_result result; \
    int status = fn; \
-   if (status != GSL_SUCCESS) { \
+   if (status == GSL_EDOM) { \
+     return GSL_NAN; \
+   } else if (status != GSL_SUCCESS) { \
      GSL_ERROR(#fn, status); \
    } ; \
    return result.val;
 
 #define EVAL_DOUBLE(fn) \
    int status = fn; \
-   if (status != GSL_SUCCESS) { \
+   if (status == GSL_EDOM) { \
+     return GSL_NAN; \
+   } else if (status != GSL_SUCCESS) { \
      GSL_ERROR(#fn, status); \
    } ; \
    return result;
