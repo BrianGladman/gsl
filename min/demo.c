c4c99f0ace74f2e23905aee5eb364deffe9b6e41
bjg <bjg>
2001-09-26 22:25:56 +0000

    fixed demo program to compile

--- min/demo.c
+++ min/demo.c
@@ -1,22 +1,3 @@
-/* min/demo.c
- * 
- * Copyright (C) 1996, 1997, 1998, 1999, 2000 Brian Gough
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 #include <stdio.h>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_math.h>
@@ -33,44 +14,46 @@ int
 main ()
 {
   int status;
-  int iterations = 0, max_iterations = 100;
+  int iter = 0, max_iter = 100;
   gsl_min_fminimizer *s;
   double m = 2.0, m_expected = M_PI;
-  double x_lower = 0.0, x_upper = 6.0;
+  double x_lower =  0, x_upper = 6.0;
   gsl_function F;
 
   F.function = &fn1;
   F.params = 0;
 
-  s = gsl_min_fminimizer_alloc (gsl_min_fminimizer_brent, &F, m, x);
+  s = gsl_min_fminimizer_alloc (gsl_min_fminimizer_brent);
+  gsl_min_fminimizer_set (s, &F, m, x_lower, x_upper);
 
   printf ("using %s method\n", gsl_min_fminimizer_name (s));
 
   printf ("%5s [%9s, %9s] %9s %9s %10s %9s\n",
-	  "iter", "lower", "upper", "min", "actual", "err", "err(est)");
+          "iter", "lower", "upper", "min", "actual",
+          "err", "err(est)");
 
   printf ("%5d [%.7f, %.7f] %.7f %.7f %+.7f %.7f\n",
-          iterations, x.lower, x.upper,
-          m, m_expected, m - m_expected, x.upper - x.lower);
+          iter, x_lower, x_upper,
+          m, m_expected, m - m_expected, x_upper - x_lower);
 
   do
     {
-      iterations++;
+      iter++;
       status = gsl_min_fminimizer_iterate (s);
 
       m = gsl_min_fminimizer_minimum (s);
       x_lower = gsl_min_fminimizer_x_lower (s);
       x_upper = gsl_min_fminimizer_x_upper (s);
 
-      status = gsl_min_test_interval (x, 0.001, 0.0);
+      status = gsl_min_test_interval (x_lower, x_upper, 0.001, 0.0);
 
       if (status == GSL_SUCCESS)
-	printf ("Converged:\n");
+        printf ("Converged:\n");
 
       printf ("%5d [%.7f, %.7f] %.7f %.7f %+.7f %.7f\n",
-	      iterations, x_lower, x_upper,
-	      m, m_expected, m - m_expected, x_upper - x_lower);
+              iter, x_lower, x_upper,
+              m, m_expected, m - m_expected, x_upper - x_lower);
     }
-  while (status == GSL_CONTINUE && iterations < max_iterations);
+  while (status == GSL_CONTINUE && iter < max_iter);
 
 }
