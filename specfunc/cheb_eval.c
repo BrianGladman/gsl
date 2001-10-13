cf91b3f6f5922ee2b83e34dbf2d360f0d603aaf4
bjg <bjg>
2001-10-13 17:45:04 +0000

    include cancellation error to prevent occassional underestimate of total error in cheb_eval

--- specfunc/cheb_eval.c
+++ specfunc/cheb_eval.c
@@ -11,14 +11,24 @@ cheb_eval_e(const cheb_series * cs,
   double y  = (2.0*x - cs->a - cs->b) / (cs->b - cs->a);
   double y2 = 2.0 * y;
 
+  double e = 0.0;
+
   for(j = cs->order; j>=1; j--) {
     double temp = d;
     d = y2*d - dd + cs->c[j];
+    e += fabs(y2*temp) + fabs(dd) + fabs(cs->c[j]);
     dd = temp;
   }
 
-  result->val = y*d - dd + 0.5 * cs->c[0];
-  result->err = GSL_DBL_EPSILON * fabs(result->val) + fabs(cs->c[cs->order]);
+  { 
+    double temp = d;
+    d = y*d - dd + 0.5 * cs->c[0];
+    e += fabs(y*temp) + fabs(dd) + 0.5 * fabs(cs->c[0]);
+  }
+
+  result->val = d;
+  result->err = GSL_DBL_EPSILON * e + fabs(cs->c[cs->order]);
+
   return GSL_SUCCESS;
 }
 
