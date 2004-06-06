2b24f3e9e1cb517b3d4287f1596c55da4a479461
bjg <bjg>
2004-06-06 17:39:14 +0000

    removed unused variables

--- linalg/test.c
+++ linalg/test.c
@@ -1668,7 +1668,6 @@ test_SV_decomp_mod_dim(const gsl_matrix * m, double eps)
   gsl_matrix * v  = gsl_matrix_alloc(M,N);
   gsl_matrix * a  = gsl_matrix_alloc(M,N);
   gsl_matrix * q  = gsl_matrix_alloc(N,N);
-  gsl_matrix * x  = gsl_matrix_alloc(N,N);
   gsl_matrix * dqt  = gsl_matrix_alloc(N,N);
   gsl_vector * d  = gsl_vector_alloc(N);
   gsl_vector * w  = gsl_vector_alloc(N);
@@ -2389,7 +2388,6 @@ int test_TDN_cyc_solve(void)
 {
   int f;
   int s = 0;
-  double actual[16];
 
   actual[0] =  3.0/2.0;
   actual[1] = -1.0/2.0;
