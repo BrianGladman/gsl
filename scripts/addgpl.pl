e901f5e80f2cd5e3a94079d9a2bc69e366459299
bjg <bjg>
2006-03-17 15:53:19 +0000

    updated copyright dates

--- scripts/addgpl.pl
+++ scripts/addgpl.pl
@@ -57,7 +57,7 @@ sub notice {
 __END__
 @FILE@
 
-Copyright (C) 1996, 1997, 1998, 1999, 2000 @AUTHORS@
+Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 @AUTHORS@
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -71,4 +71,4 @@ General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
