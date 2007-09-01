42a81204ff17679ee1fe718018202b70af423191
bjg <bjg>
2007-09-01 16:35:58 +0000

    fix memory leak

--- randist/test.c
+++ randist/test.c
@@ -522,6 +522,7 @@ integrate (pdf_func * pdf, double a, double b)
   f.params = pdf;
   gsl_integration_workspace * w = gsl_integration_workspace_alloc (n);
   gsl_integration_qags (&f, a, b, 1e-16, 1e-4, n, w, &result, &abserr);
+  gsl_integration_workspace_free (w);
   return result;
 }
 
