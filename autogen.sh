ef72a4f0315c5e0319e38ecc5c952487411d2ecd
Brian Gough <bjg@gnu.org>
2009-05-09 12:21:25 +0100

    added message about autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
+echo "If you use a recent version of autotools, this script is obsolete"
+echo "Just run autoreconf -i instead"
+echo
+
 # Run this to generate all the auto-generated files needed by the GNU
 # configure program
-
 libtoolize --automake
 aclocal
 autoheader
