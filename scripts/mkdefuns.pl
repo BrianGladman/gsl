7ab6e45610a3ed730634b30204390abfa779bb0e
Brian Gough <bjg@gnu.org>
2009-06-05 12:03:32 +0100

    try to produce cleaner defuns from headers

--- scripts/mkdefuns.pl
+++ scripts/mkdefuns.pl
@@ -14,9 +14,16 @@ while (<>) {
     s/;$//;
     s/\s+\)/\)/g;
     s/(\w)\(/$1 \(/g;
-    next if !/\(/ ;
+    next if !/\(.*\)\s*$/ ;
+    s/\b_(gsl_\w+_view)/\1/g;
     s/(\w+)(\[|,|\))/\@var{$1}$2/g;
-    s/\@var\{\*/*\@var\{/g ;
+    s/\s*\,(\w)/, $1/g;
+    s/\@var\{\*/* \@var\{/g ;
+    s/\*\@var\{/* \@var\{/g ;
+    s/\*(\S)/* $1/g ;
+    s/\@var{void}/void/g;
+    s/\(\s+/(/g;
+    next if /^([a-z]+ )+gsl_(block|vector|matrix|stats|sort|permute|sort_vector|permute_vector)_(u?int|u?short|u?long|u?char|float|long_double|complex_float|complex_double|complex_long_double)/;
     print "\@deftypefun $_\n";
     print "\@end deftypefun\n\n" ;
 }
