d2452c0f38c1e75083d2dc4d71661b09afe9d84b
Brian Gough <bjg@gnu.org>
2010-10-23 14:43:43 +0100

    added note about configure maintainer mode

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
 echo "If you use a recent version of autotools, this script is obsolete"
-echo "Just run autoreconf -i -f -v instead"
-echo
+echo "Just run autoreconf -i -f -v"
+echo "followed by ./configure --enable-maintainer-mode"
+echo 
 
 # Run this to generate all the auto-generated files needed by the GNU
 # configure program
