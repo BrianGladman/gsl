254b03f545b71285afe09f2537d101c2ab082005
Patrick Alken <alken@colorado.edu>
2014-04-03 15:50:03 -0600

    doc improvement for gsl_interp_steffen

--- interpolation/test.c
+++ interpolation/test.c
@@ -1,6 +1,7 @@
 /* interpolation/test.c
  * 
- * Copyright (C) 1996, 1997, 1998, 1999, 2000 Gerard Jungman
+ * Copyright (C) 1996, 1997, 1998, 1999, 2000, 2014 Gerard Jungman
+ * Copyright (C) 2014 Jean-François Caron
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
