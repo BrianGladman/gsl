833f6db7052345e8a27eac0d5a75a40ccb1add47
Patrick Alken <alken@colorado.edu>
2015-10-25 13:31:42 -0600

    test release gsl-1.99.90

--- scripts/runvalgrind.sh
+++ scripts/runvalgrind.sh
@@ -3,10 +3,16 @@
 # Run all tests under valgrind/memcheck
 #
 # First, compile the library with:
-# > make CFLAGS="-g -Wall" check
+# > ./configure --disable-shared CFLAGS="-g -Wall"
+# > make ; make check
 # to compile the library and tests without optimization
 
-for testprog in `ls */test`; do
+outfile="valgrind.out"
+rm -f $outfile
+
+for testprog in $(ls */test); do
   echo "Running valgrind on ${testprog}"
-  valgrind ${testprog}
+  valgrind ${testprog} >> $outfile 2>&1
 done
+
+echo "output file is $outfile"
