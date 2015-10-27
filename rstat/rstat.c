4542c30c19c102171a1efc4f3754e7aa92b393af
Patrick Alken <alken@colorado.edu>
2015-10-27 11:40:45 -0600

    minor update to rstat

--- rstat/rstat.c
+++ rstat/rstat.c
@@ -69,7 +69,7 @@ int
 gsl_rstat_add(const double x, gsl_rstat_workspace *w)
 {
   double delta = x - w->mean;
-  double delta_n, delta_nsq, term1;
+  double delta_n, delta_nsq, term1, n;
 
   /* update min and max */
   if (w->n == 0)
@@ -86,14 +86,14 @@ gsl_rstat_add(const double x, gsl_rstat_workspace *w)
     }
 
   /* update mean and variance */
-  ++(w->n);
-  delta_n = delta / w->n;
+  n = (double) ++(w->n);
+  delta_n = delta / n;
   delta_nsq = delta_n * delta_n;
-  term1 = delta * delta_n * (w->n - 1.0);
+  term1 = delta * delta_n * (n - 1.0);
   w->mean += delta_n;
-  w->M4 += term1 * delta_nsq * (w->n * w->n - 3.0 * w->n + 3.0) +
+  w->M4 += term1 * delta_nsq * (n * n - 3.0 * n + 3.0) +
            6.0 * delta_nsq * w->M2 - 4.0 * delta_n * w->M3;
-  w->M3 += term1 * delta_n * (w->n - 2.0) - 3.0 * delta_n * w->M2;
+  w->M3 += term1 * delta_n * (n - 2.0) - 3.0 * delta_n * w->M2;
   w->M2 += term1;
 
   /* update median */
@@ -173,7 +173,8 @@ gsl_rstat_kurtosis(gsl_rstat_workspace *w)
 {
   if (w->n > 0)
     {
-      double fac = (w->n - 1.0) * (w->n - 1.0) / (double)w->n;
+      double n = (double) w->n;
+      double fac = ((n - 1.0) / n) * (n - 1.0);
       return ((fac * w->M4) / (w->M2 * w->M2) - 3.0);
     }
   else
